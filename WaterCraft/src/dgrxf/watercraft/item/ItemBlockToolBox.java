8fa1bb126045ddb3b8e72abf8e01d1bed0d08fba
DrunkMafia <DrunkMafia@gmx.com>
2013-11-08 18:18:20 +0000

    item version of toolbox created

---
+++ WaterCraft/src/dgrxf/watercraft/item/ItemBlockToolBox.java
@@ -0,0 +1,10 @@
+package dgrxf.watercraft.item;
+
+import net.minecraft.item.ItemBlock;
+
+public class ItemBlockToolBox extends ItemBlock {
+
+	public ItemBlockToolBox(int id) {
+		super(id);
+	}
+}
