b6aff6a52dec3b69170580920978219c24d75034
DrunkMafia <DrunkMafia@gmx.com>
2013-11-08 21:07:41 +0000

    Created Toolbox Container

---
+++ WaterCraft/src/dgrxf/watercraft/client/gui/container/ToolboxContainer.java
@@ -0,0 +1,30 @@
+package dgrxf.watercraft.client.gui.container;
+
+import dgrxf.watercraft.tileentity.WCTileEntityToolBox;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.entity.player.InventoryPlayer;
+import net.minecraft.inventory.Container;
+import net.minecraft.inventory.Slot;
+
+public class ToolboxContainer extends Container {
+
+	public ToolboxContainer(InventoryPlayer invPlayer, WCTileEntityToolBox te) {
+		
+		for (int x = 0; x < 9; x++) {
+			addSlotToContainer(new Slot(invPlayer, x, 8 + 18 * x, 99));
+		}
+		
+		for (int y = 0; y < 3; y++) {
+			for (int x = 0; x < 9; x++) {
+				addSlotToContainer(new Slot(invPlayer, x + y * 9 + 9, 8 + 18 * x, 40 + y * 19));
+			}
+		}
+		
+	}
+	
+	@Override
+	public boolean canInteractWith(EntityPlayer entityplayer) {
+		return true;
+	}
+
+}
