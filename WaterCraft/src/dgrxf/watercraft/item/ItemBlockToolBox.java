d5e0ee7f2f26477d8530aee52007d9ec15d719a6
xandayn <xandayn101@hotmail.com>
2013-11-08 14:16:11 -0700

    Added ToolBox to creative tab

--- WaterCraft/src/dgrxf/watercraft/item/ItemBlockToolBox.java
+++ WaterCraft/src/dgrxf/watercraft/item/ItemBlockToolBox.java
@@ -1,10 +1,12 @@
 package dgrxf.watercraft.item;
 
+import dgrxf.watercraft.Watercraft;
 import net.minecraft.item.ItemBlock;
 
 public class ItemBlockToolBox extends ItemBlock {
 
 	public ItemBlockToolBox(int id) {
 		super(id);
+		this.setCreativeTab(Watercraft.tab);
 	}
 }
