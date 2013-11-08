cc6ab1da8671bb7cc8691569f27642f572dec9f2
DrunkMafia <DrunkMafia@gmx.com>
2013-11-08 18:17:35 +0000

    added toolbox entries

--- WaterCraft/src/dgrxf/watercraft/lib/RenderInfo.java
+++ WaterCraft/src/dgrxf/watercraft/lib/RenderInfo.java
@@ -16,5 +16,6 @@ public class RenderInfo {
     public static int BUOY_RENDER_ID;
     
     public static final ResourceLocation CONTROL_UNIT_TEXTURE_LOCATION = new ResourceLocation(ModInfo.MODID, "textures/models/ControlUnit2.png");
+    public static final ResourceLocation TOOLBOX_TEXTURE_LOCATION = new ResourceLocation(ModInfo.MODID, "textures/models/Toolbox.png");
     
 }
