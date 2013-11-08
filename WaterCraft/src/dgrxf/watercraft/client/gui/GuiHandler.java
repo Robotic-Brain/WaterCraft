5832ea48a8576ef5f144f8a5fc9832fb4d5114c3
DrunkMafia <DrunkMafia@gmx.com>
2013-11-08 21:07:29 +0000

    Added toolbox GUI

--- WaterCraft/src/dgrxf/watercraft/client/gui/GuiHandler.java
+++ WaterCraft/src/dgrxf/watercraft/client/gui/GuiHandler.java
@@ -1,10 +1,14 @@
 package dgrxf.watercraft.client.gui;
 
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.tileentity.TileEntity;
 import net.minecraft.world.World;
 import cpw.mods.fml.common.network.IGuiHandler;
 import cpw.mods.fml.common.network.NetworkRegistry;
 import dgrxf.watercraft.Watercraft;
+import dgrxf.watercraft.client.gui.container.ToolboxContainer;
+import dgrxf.watercraft.client.gui.interfaces.ToolBoxGUI;
+import dgrxf.watercraft.tileentity.WCTileEntityToolBox;
 
 public class GuiHandler implements IGuiHandler {
     
@@ -14,11 +18,27 @@ public class GuiHandler implements IGuiHandler {
     
     @Override
     public Object getServerGuiElement(int ID, EntityPlayer player, World world, int x, int y, int z) {
+        switch(ID){
+        	case 0:
+        		TileEntity te = world.getBlockTileEntity(x, y, z);
+        		if(te != null && te instanceof WCTileEntityToolBox){
+        			return new ToolboxContainer(player.inventory, (WCTileEntityToolBox)te);
+        		}
+        		break;
+        }
         return null;
     }
     
     @Override
     public Object getClientGuiElement(int ID, EntityPlayer player, World world, int x, int y, int z) {
-        return null;
+    	switch(ID){
+    		case 0:
+    			TileEntity te = world.getBlockTileEntity(x, y, z);
+        		if(te != null && te instanceof WCTileEntityToolBox){
+        			return new ToolBoxGUI(player.inventory, (WCTileEntityToolBox)te);
+        		}
+        		break;
+    	}
+    	return null;
     }
 }
