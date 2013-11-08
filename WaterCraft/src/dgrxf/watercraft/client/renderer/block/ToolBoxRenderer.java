2c82a1b1915d132d3403a66cf43f9fc774c78dbe
DrunkMafia <DrunkMafia@gmx.com>
2013-11-08 18:18:27 +0000

    Toolbox renderer created

---
+++ WaterCraft/src/dgrxf/watercraft/client/renderer/block/ToolBoxRenderer.java
@@ -0,0 +1,33 @@
+package dgrxf.watercraft.client.renderer.block;
+
+import org.lwjgl.opengl.GL11;
+
+import dgrxf.watercraft.lib.RenderInfo;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.renderer.tileentity.TileEntitySpecialRenderer;
+import net.minecraft.tileentity.TileEntity;
+import net.minecraftforge.client.model.AdvancedModelLoader;
+import net.minecraftforge.client.model.IModelCustom;
+
+/**
+ * Class Made By: Drunk Mafia
+ * 
+ * Class Last Edited By:Drunk Mafia Class Last Edited On:11/08/2013 MM/DD/YYYYY
+ * 
+ */
+
+public class ToolBoxRenderer extends TileEntitySpecialRenderer {
+	private IModelCustom modelBuoy = AdvancedModelLoader.loadModel("/assets/watercraft/models/toolbox.obj");
+    
+    @Override
+    public void renderTileEntityAt(TileEntity tileentity, double x, double y, double z, float partialTickTime) {
+        GL11.glPushMatrix();
+        
+        GL11.glTranslatef((float) x + 0.5F, (float) y - 0.5F, (float) z + 0.5F);
+        Minecraft.getMinecraft().renderEngine.bindTexture(RenderInfo.BUOY_TEXTURE_LOCATION);
+        
+        modelBuoy.renderAll();
+        
+        GL11.glPopMatrix();
+    }
+}
