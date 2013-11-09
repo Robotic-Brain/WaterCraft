009a7f691921b6059e07721b294cf358c6139882
TheDrunkMafia <sean.hall412@gmail.com>
2013-11-09 23:41:19 +0000

    Now drops inventory

--- WaterCraft/src/dgrxf/watercraft/block/ToolBoxBlock.java
+++ WaterCraft/src/dgrxf/watercraft/block/ToolBoxBlock.java
@@ -1,8 +1,9 @@
 package dgrxf.watercraft.block;
 
 import net.minecraft.block.material.Material;
-import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.entity.item.EntityItem;
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.inventory.IInventory;
 import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.nbt.NBTTagList;
@@ -13,7 +14,6 @@ import dgrxf.watercraft.Watercraft;
 import dgrxf.watercraft.client.gui.GuiHandler;
 import dgrxf.watercraft.lib.BlockInfo;
 import dgrxf.watercraft.lib.RenderInfo;
-import dgrxf.watercraft.network.PacketHandler;
 import dgrxf.watercraft.tileentity.WCTileEntityToolBox;
 
 /**
@@ -86,10 +86,44 @@ public class ToolBoxBlock extends WCBlock {
 			}
 		}
 		compound.setTag("Items", items);
+		compound.setString("playerName", tile.getPlayerName());
 		toolBox.setTagCompound(compound);
 		
 		player.inventory.mainInventory[player.inventory.currentItem] = toolBox;
 	}
+    
+    @Override
+    public void breakBlock(World world, int x, int y, int z, int id, int meta) {
+    	TileEntity te = world.getBlockTileEntity(x, y, z);
+		if (te != null && te instanceof IInventory) {
+			IInventory inventory = (IInventory)te;
+			
+			for (int i = 0; i < inventory.getSizeInventory(); i++) {
+				ItemStack stack = inventory.getStackInSlotOnClosing(i);
+				
+				if (stack != null) {
+					float spawnX = x + world.rand.nextFloat();
+					float spawnY = y + world.rand.nextFloat();
+					float spawnZ = z + world.rand.nextFloat();
+					
+					EntityItem droppedItem = new EntityItem(world, spawnX, spawnY, spawnZ, stack);
+					
+					float mult = 0.05F;
+					
+					droppedItem.motionX = (-0.5F + world.rand.nextFloat()) * mult;
+					droppedItem.motionY = (4 + world.rand.nextFloat()) * mult;
+					droppedItem.motionZ = (-0.5F + world.rand.nextFloat()) * mult;
+					
+					world.spawnEntityInWorld(droppedItem);
+				}
+			}
+		}
+		float spawnX = x + world.rand.nextFloat();
+		float spawnY = y + world.rand.nextFloat();
+		float spawnZ = z + world.rand.nextFloat();
+		world.spawnEntityInWorld(new EntityItem(world, spawnX, spawnY, spawnZ, new ItemStack(this)));
+		super.breakBlock(world, x, y, z, id, meta);
+    }
 
 	@Override
     public boolean isOpaqueCube() {
