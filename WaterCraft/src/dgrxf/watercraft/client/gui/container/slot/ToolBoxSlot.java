89a733672f3f6a42a9409639ac0a00cc3b8b976c
TheDrunkMafia <drunkmafia@gmx.com>
2013-11-09 17:55:53 +0000

    Created toolboxslot

---
+++ WaterCraft/src/dgrxf/watercraft/client/gui/container/slot/ToolBoxSlot.java
@@ -0,0 +1,33 @@
+package dgrxf.watercraft.client.gui.container.slot;
+
+import net.minecraft.inventory.IInventory;
+import net.minecraft.inventory.Slot;
+import net.minecraft.item.ItemBow;
+import net.minecraft.item.ItemBucket;
+import net.minecraft.item.ItemFishingRod;
+import net.minecraft.item.ItemFlintAndSteel;
+import net.minecraft.item.ItemShears;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.ItemSword;
+import net.minecraft.item.ItemTool;
+
+public class ToolBoxSlot extends Slot{
+
+	public ToolBoxSlot(IInventory inv, int id, int x, int y) {
+		super(inv, id, x, y);
+	}
+	
+	@Override
+	public boolean isItemValid(ItemStack itemstack) {
+		if(itemstack.getItem() instanceof ItemTool){	return true;
+		}else if(itemstack.getItem() instanceof ItemBow){ return true;
+		}else if(itemstack.getItem() instanceof ItemBucket){ return true;
+		}else if(itemstack.getItem() instanceof ItemSword){ return true;
+		}else if(itemstack.getItem() instanceof ItemShears){ return true;
+		}else if(itemstack.getItem() instanceof ItemFlintAndSteel){ return true;
+		}else if(itemstack.getItem() instanceof ItemFishingRod){ return true;
+		}
+		return false;
+	}
+
+}
