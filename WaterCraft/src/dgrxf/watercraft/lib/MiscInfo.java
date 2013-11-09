c6710cddcb1a645c6b016a59517ee9afa1ca478f
TheDrunkMafia <sean.hall412@gmail.com>
2013-11-09 19:04:55 +0000

    Created miscinfo

---
+++ WaterCraft/src/dgrxf/watercraft/lib/MiscInfo.java
@@ -0,0 +1,11 @@
+package dgrxf.watercraft.lib;
+
+public class MiscInfo {
+	
+	public static final String CATEGORY = "Misc";
+	
+	public static final String BOUY_KEY = "Bouy Search Range";
+	
+	public static final int BOUY_DEFAULT_RANGE = 10;
+    public static int BOUY_RANGE;
+}
