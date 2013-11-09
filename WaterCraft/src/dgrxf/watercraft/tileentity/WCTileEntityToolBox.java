a5eed91f30475225bc22918345e0acbaf9de9c2f
TheDrunkMafia <drunkmafia@gmx.com>
2013-11-09 16:39:58 +0000

    Added players name code

--- WaterCraft/src/dgrxf/watercraft/tileentity/WCTileEntityToolBox.java
+++ WaterCraft/src/dgrxf/watercraft/tileentity/WCTileEntityToolBox.java
@@ -1,16 +1,31 @@
 package dgrxf.watercraft.tileentity;
 
+import dgrxf.watercraft.network.PacketHandler;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.inventory.IInventory;
 import net.minecraft.item.ItemStack;
+import net.minecraft.nbt.NBTTagCompound;
+import net.minecraft.nbt.NBTTagList;
+import net.minecraft.network.INetworkManager;
+import net.minecraft.network.packet.Packet;
+import net.minecraft.network.packet.Packet132TileEntityData;
 import net.minecraft.tileentity.TileEntity;
 
 public class WCTileEntityToolBox extends TileEntity implements IInventory{
 
 	private ItemStack[] inventory;
+	private String playerName;
 	
 	public WCTileEntityToolBox() {
 		inventory = new ItemStack[getSizeInventory()];
+	}
+	
+	public void setPlayerName(String playerName) {
+		this.playerName = playerName;
+	}
+	
+	public String getPlayerName() {
+		return playerName;
 	}
 	
 	@Override
@@ -24,8 +39,19 @@ public class WCTileEntityToolBox extends TileEntity implements IInventory{
 	}
 
 	@Override
-	public ItemStack decrStackSize(int i, int j) {
-		return null;
+	public ItemStack decrStackSize(int i, int count) {
+		ItemStack itemstack = getStackInSlot(i);
+		
+		if (itemstack != null) {
+			if (itemstack.stackSize <= count) {
+				setInventorySlotContents(i, null);
+			}else{
+				itemstack = itemstack.splitStack(count);
+				onInventoryChanged();
+			}
+		}
+
+		return itemstack;
 	}
 
 	@Override
@@ -36,7 +62,11 @@ public class WCTileEntityToolBox extends TileEntity implements IInventory{
 
 	@Override
 	public void setInventorySlotContents(int i, ItemStack itemstack) {
-		inventory[i] = itemstack;		
+		inventory[i] = itemstack;
+		
+		if (itemstack != null && itemstack.stackSize > getInventoryStackLimit()) {
+			itemstack.stackSize = getInventoryStackLimit();
+		}	
 	}
 
 	@Override
@@ -67,5 +97,38 @@ public class WCTileEntityToolBox extends TileEntity implements IInventory{
 	@Override
 	public boolean isItemValidForSlot(int i, ItemStack itemstack) {
 		return false;
+	}
+	
+	@Override
+	public void writeToNBT(NBTTagCompound compound) {
+		super.writeToNBT(compound);
+		NBTTagList items = new NBTTagList();
+		
+		for (int i = 0; i < getSizeInventory(); i++) {		
+			ItemStack stack = getStackInSlot(i);
+			
+			if (stack != null) {
+				NBTTagCompound item = new NBTTagCompound();
+				item.setByte("Slot", (byte)i);
+				stack.writeToNBT(item);
+				items.appendTag(item);
+			}
+		}
+		compound.setTag("Items", items);
+	}
+	
+	@Override
+	public void readFromNBT(NBTTagCompound compound) {
+		super.readFromNBT(compound);
+		NBTTagList items = compound.getTagList("Items");
+		
+		for (int i = 0; i < items.tagCount(); i++) {
+			NBTTagCompound item = (NBTTagCompound)items.tagAt(i);
+			int slot = item.getByte("Slot");
+			
+			if (slot >= 0 && slot < getSizeInventory()) {
+				setInventorySlotContents(slot, ItemStack.loadItemStackFromNBT(item));
+			}
+		}
 	}
 }
