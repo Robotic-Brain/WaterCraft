63f92891451502bf232cb05615f8d5c8a879537d
TheDrunkMafia <drunkmafia@gmx.com>
2013-11-09 16:39:49 +0000

    Trying to send a string to the client

--- WaterCraft/src/dgrxf/watercraft/network/PacketHandler.java
+++ WaterCraft/src/dgrxf/watercraft/network/PacketHandler.java
@@ -1,15 +1,63 @@
 package dgrxf.watercraft.network;
 
+import java.io.ByteArrayOutputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+
+import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.network.INetworkManager;
 import net.minecraft.network.packet.Packet250CustomPayload;
+import net.minecraft.world.World;
+
+import com.google.common.io.ByteArrayDataInput;
+import com.google.common.io.ByteStreams;
+
+import cpw.mods.fml.common.FMLLog;
 import cpw.mods.fml.common.network.IPacketHandler;
+import cpw.mods.fml.common.network.PacketDispatcher;
 import cpw.mods.fml.common.network.Player;
+import dgrxf.watercraft.Watercraft;
+import dgrxf.watercraft.lib.ModInfo;
+import dgrxf.watercraft.tileentity.WCTileEntityToolBox;
 
 public class PacketHandler implements IPacketHandler {
     
     @Override
     public void onPacketData(INetworkManager manager, Packet250CustomPayload packet, Player player) {
-        
+    	ByteArrayDataInput reader = ByteStreams.newDataInput(packet.data);
+		
+		EntityPlayer entityPlayer = (EntityPlayer)player;
+		World world = entityPlayer.worldObj;
+		
+		byte packetId = reader.readByte();
+		
+		switch (packetId) {
+			case 0:
+				WCTileEntityToolBox tile = (WCTileEntityToolBox) world.getBlockTileEntity(reader.readInt(), reader.readInt(), reader.readInt());
+				String name = reader.readUTF().toString();
+				Watercraft.printToPlayer(name);
+				if(tile != null) tile.setPlayerName(name); else System.out.println("Tile does not exist?!");
+				break;
+			default:
+				System.out.println(ModInfo.getMODID() + " Invalid packet recived!");
+		}
     }
     
+    public static void toolboxSendPlayerName(int x, int y, int z, EntityPlayer player){
+    	ByteArrayOutputStream byteStream = new ByteArrayOutputStream();
+		DataOutputStream dataStream = new DataOutputStream(byteStream);
+
+		try {
+			dataStream.writeByte((byte)0);
+			dataStream.writeInt(x);
+			dataStream.writeInt(y);
+			dataStream.writeInt(z);
+			dataStream.writeUTF(player.username);
+			
+			PacketDispatcher.sendPacketToAllPlayers(PacketDispatcher.getPacket(ModInfo.CHANNEL, byteStream.toByteArray()));
+		}catch(IOException e) {
+			FMLLog.warning(ModInfo.getMODID() + " Failed to send player name packet for the ToolBox, please report this to the mod Author", e);
+			e.printStackTrace();
+		}
+    }
 }
