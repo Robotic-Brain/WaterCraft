ca6719b8209d6c31f1426221c4870f939bc5b66b
TheDrunkMafia <drunkmafia@gmx.com>
2013-11-09 14:46:23 +0000

    Now Implements IInventory

--- WaterCraft/src/dgrxf/watercraft/tileentity/WCTileEntityToolBox.java
+++ WaterCraft/src/dgrxf/watercraft/tileentity/WCTileEntityToolBox.java
@@ -1,7 +1,71 @@
 package dgrxf.watercraft.tileentity;
 
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.inventory.IInventory;
+import net.minecraft.item.ItemStack;
 import net.minecraft.tileentity.TileEntity;
 
-public class WCTileEntityToolBox extends TileEntity{
+public class WCTileEntityToolBox extends TileEntity implements IInventory{
 
+	private ItemStack[] inventory;
+	
+	public WCTileEntityToolBox() {
+		inventory = new ItemStack[getSizeInventory()];
+	}
+	
+	@Override
+	public int getSizeInventory() {
+		return 9;
+	}
+
+	@Override
+	public ItemStack getStackInSlot(int i) {
+		return inventory[i];
+	}
+
+	@Override
+	public ItemStack decrStackSize(int i, int j) {
+		return null;
+	}
+
+	@Override
+	public ItemStack getStackInSlotOnClosing(int i) {
+		ItemStack slot = getStackInSlot(i);
+		return slot;
+	}
+
+	@Override
+	public void setInventorySlotContents(int i, ItemStack itemstack) {
+		inventory[i] = itemstack;		
+	}
+
+	@Override
+	public String getInvName() {
+		return "WCToolBoxGUI";
+	}
+
+	@Override
+	public boolean isInvNameLocalized() {
+		return true;
+	}
+
+	@Override
+	public int getInventoryStackLimit() {
+		return 64;
+	}
+
+	@Override
+	public boolean isUseableByPlayer(EntityPlayer entityplayer) {
+		return true;
+	}
+
+	@Override
+	public void openChest() {}
+	@Override
+	public void closeChest() {}
+
+	@Override
+	public boolean isItemValidForSlot(int i, ItemStack itemstack) {
+		return false;
+	}
 }
