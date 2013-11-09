260daaa23cbda7c3b551b74545b1f96acda1e7b1
TheDrunkMafia <drunkmafia@gmx.com>
2013-11-09 16:38:39 +0000

    Added ability to pick up the toolbox

--- WaterCraft/src/dgrxf/watercraft/block/ToolBoxBlock.java
+++ WaterCraft/src/dgrxf/watercraft/block/ToolBoxBlock.java
@@ -1,7 +1,11 @@
 package dgrxf.watercraft.block;
 
 import net.minecraft.block.material.Material;
+import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.item.ItemStack;
+import net.minecraft.nbt.NBTTagCompound;
+import net.minecraft.nbt.NBTTagList;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.world.World;
 import cpw.mods.fml.common.network.FMLNetworkHandler;
@@ -9,6 +13,7 @@ import dgrxf.watercraft.Watercraft;
 import dgrxf.watercraft.client.gui.GuiHandler;
 import dgrxf.watercraft.lib.BlockInfo;
 import dgrxf.watercraft.lib.RenderInfo;
+import dgrxf.watercraft.network.PacketHandler;
 import dgrxf.watercraft.tileentity.WCTileEntityToolBox;
 
 public class ToolBoxBlock extends WCBlock {
@@ -31,14 +36,48 @@ public class ToolBoxBlock extends WCBlock {
     
     @Override
 	public boolean onBlockActivated(World world, int x, int y, int z, EntityPlayer player, int side, float hitX, float hitY, float hitZ) {
-		if (!world.isRemote) {
-			FMLNetworkHandler.openGui(player, Watercraft.instance, GuiHandler.TOOLBOX_GUI_ID, world, x, y, z);			
+		if (world.isRemote) return true;
+
+		if(!player.isSneaking()) FMLNetworkHandler.openGui(player, Watercraft.instance, GuiHandler.TOOLBOX_GUI_ID, world, x, y, z); else{
+			if(player.getCurrentEquippedItem() == null) pickUpToolBox(world, x, y, z, player);
 		}
-	
 		return true;
 	}
     
-    @Override
+    private void pickUpToolBox(World world, int x, int y, int z, EntityPlayer player) {
+    	WCTileEntityToolBox tile = (WCTileEntityToolBox) world.getBlockTileEntity(x, y, z);
+    	
+    	world.destroyBlock(x, y, z, false);
+    	
+    	int size = tile.getSizeInventory();
+    	ItemStack[] tempInv = new ItemStack[size];
+    	
+    	for(int i = 0; i < size; i++){
+    		if(tile.getStackInSlot(i) != null)
+    			tempInv[i] = tile.getStackInSlot(i);
+    	}
+    	
+    	ItemStack toolBox = new ItemStack(this);
+    	NBTTagCompound compound = new NBTTagCompound();
+    	NBTTagList items = new NBTTagList();
+		
+		for (int i = 0; i < tempInv.length; i++) {		
+			ItemStack slot = tempInv[i];
+			
+			if (slot != null) {
+				NBTTagCompound item = new NBTTagCompound();
+				item.setByte("Slot", (byte)i);
+				slot.writeToNBT(item);
+				items.appendTag(item);
+			}
+		}
+		compound.setTag("Items", items);
+		toolBox.setTagCompound(compound);
+		
+		player.inventory.mainInventory[player.inventory.currentItem] = toolBox;
+	}
+
+	@Override
     public boolean isOpaqueCube() {
         return false;
     }
