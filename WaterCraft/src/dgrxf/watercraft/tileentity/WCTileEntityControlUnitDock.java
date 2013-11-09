835d04db77c00c7631c39ee500af0235f3e16658
TheDrunkMafia <sean.hall412@gmail.com>
2013-11-09 18:35:52 +0000

    Moved detection code to multiblock class

--- WaterCraft/src/dgrxf/watercraft/tileentity/WCTileEntityControlUnitDock.java
+++ WaterCraft/src/dgrxf/watercraft/tileentity/WCTileEntityControlUnitDock.java
@@ -21,7 +21,6 @@ public class WCTileEntityControlUnitDock extends WCTileEntityBuoy {
     private boolean firstRun, multiBlockFormed;
     private int direction;
     private int updateTimer;
-    private int[][][] blocks;
     
     public WCTileEntityControlUnitDock() {
         firstRun = true;
@@ -96,44 +95,7 @@ public class WCTileEntityControlUnitDock extends WCTileEntityBuoy {
     }
     
     public boolean checkForMultiBlock() {
-        int tempX = xCoord;
-        int tempY = yCoord - 1;
-        int tempZ = zCoord;
-        int val = 0;
-        switch (direction-2) {
-            case 0:
-                tempZ -= 3;
-                break;
-            case 1:
-                tempZ += 3;
-                break;
-            case 2:
-                tempX -= 3;
-                break;
-            case 3:
-                tempX += 3;
-                break;
-        }
-        blocks = new int[2][5][5];
-        int yL = 0;
-        int inc = 0;
-        for (int y = -1; y < 1; y++) {
-            int xL = 0, zL = 0;
-            for (int i = tempX - 2; i <= tempX + 2; i++) {
-                zL = 0;
-                for (int j = tempZ - 2; j <= tempZ + 2; j++) {
-                    inc++;
-                    if (worldObj.blockExists(i, tempY + y, j)) {
-                        blocks[yL][xL][zL] = worldObj.getBlockId(i, tempY + y, j);
-                        int temp = tempY + y;
-                    }
-                    zL++;
-                }
-                xL++;
-            }
-            yL++;
-        }
         firstRun = false;
-        return MultiBlockInfo.dock.isPatternCorrect(direction, blocks);
+        return MultiBlockInfo.dock.getMultiBlock(getWorldObj(), xCoord, yCoord, zCoord, direction);
     }
 }
