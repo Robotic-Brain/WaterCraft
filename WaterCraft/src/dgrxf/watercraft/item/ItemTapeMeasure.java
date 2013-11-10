3a5e231cc2e80a2ca7bf5d3e6b22223b12e51808
FrodCube <frodcube@gmail.com>
2013-11-10 19:12:18 +0100

    Tape texture just because

--- WaterCraft/src/dgrxf/watercraft/item/ItemTapeMeasure.java
+++ WaterCraft/src/dgrxf/watercraft/item/ItemTapeMeasure.java
@@ -1,10 +1,14 @@
 package dgrxf.watercraft.item;
 
+import cpw.mods.fml.relauncher.Side;
+import cpw.mods.fml.relauncher.SideOnly;
 import net.minecraft.client.Minecraft;
+import net.minecraft.client.renderer.texture.IconRegister;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NBTTagCompound;
+import net.minecraft.util.Icon;
 import net.minecraft.world.World;
 import dgrxf.watercraft.Watercraft;
 import dgrxf.watercraft.lib.ItemInfo;
@@ -42,5 +46,11 @@ public class ItemTapeMeasure extends Item{
 		
 		stack.setTagCompound(tag);
 		return false;
+	}
+	
+	@Override
+	@SideOnly(Side.CLIENT)
+	public void registerIcons(IconRegister register) {
+		itemIcon = register.registerIcon("Watercraft:tape");
 	}
 }
