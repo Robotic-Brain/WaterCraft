2e5d13f2ec3a8891a6713e67c0ad0d1812fe3cdc
TheDrunkMafia <sean.hall412@gmail.com>
2013-11-10 15:47:24 +0000

    Updated GUI to work with the toolbox

--- WaterCraft/src/dgrxf/watercraft/client/gui/GuiHandler.java
+++ WaterCraft/src/dgrxf/watercraft/client/gui/GuiHandler.java
@@ -1,6 +1,7 @@
 package dgrxf.watercraft.client.gui;
 
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.item.ItemStack;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.world.World;
 import cpw.mods.fml.common.network.IGuiHandler;
@@ -8,6 +9,7 @@ import cpw.mods.fml.common.network.NetworkRegistry;
 import dgrxf.watercraft.Watercraft;
 import dgrxf.watercraft.client.gui.container.ToolboxContainer;
 import dgrxf.watercraft.client.gui.interfaces.ToolBoxGUI;
+import dgrxf.watercraft.lib.BlockInfo;
 import dgrxf.watercraft.tileentity.WCTileEntityToolBox;
 
 public class GuiHandler implements IGuiHandler {
@@ -21,6 +23,7 @@ public class GuiHandler implements IGuiHandler {
     
     @Override
     public Object getServerGuiElement(int id, EntityPlayer player, World world, int x, int y, int z) {
+<<<<<<< HEAD
         switch (id) {
             case TOOLBOX_GUI_ID:
                 TileEntity te = world.getBlockTileEntity(x, y, z);
@@ -28,12 +31,22 @@ public class GuiHandler implements IGuiHandler {
                     return new ToolboxContainer(player.inventory, (WCTileEntityToolBox) te);
                 }
                 break;
+=======
+        switch(id){
+        	case TOOLBOX_GUI_ID:
+        		TileEntity te = world.getBlockTileEntity(x, y, z);
+        		if(te instanceof WCTileEntityToolBox || player.inventory.getCurrentItem().getItem().itemID == BlockInfo.TOOLBOX_ID){
+        			return new ToolboxContainer(player.inventory, te != null ? (WCTileEntityToolBox)te : null);
+        		}
+        		break;
+>>>>>>> Commit
         }
         return null;
     }
     
     @Override
     public Object getClientGuiElement(int id, EntityPlayer player, World world, int x, int y, int z) {
+<<<<<<< HEAD
         switch (id) {
             case TOOLBOX_GUI_ID:
                 TileEntity te = world.getBlockTileEntity(x, y, z);
@@ -43,5 +56,16 @@ public class GuiHandler implements IGuiHandler {
                 break;
         }
         return null;
+=======
+    	switch(id){
+    		case TOOLBOX_GUI_ID:
+    			TileEntity te = world.getBlockTileEntity(x, y, z);
+        		if(te instanceof WCTileEntityToolBox || player.inventory.getCurrentItem().getItem().itemID == BlockInfo.TOOLBOX_ID){
+        			return new ToolBoxGUI(player.inventory, te != null ? (WCTileEntityToolBox)te : null);
+        		}
+        		break;
+    	}
+    	return null;
+>>>>>>> Commit
     }
 }
