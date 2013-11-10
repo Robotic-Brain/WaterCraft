1653c2e97e10fde9a0e1bb43021035d7f469b7c4
TheDrunkMafia <sean.hall412@gmail.com>
2013-11-10 15:46:51 +0000

    Changed the activation code

--- WaterCraft/src/dgrxf/watercraft/block/ToolBoxBlock.java
+++ WaterCraft/src/dgrxf/watercraft/block/ToolBoxBlock.java
@@ -10,10 +10,17 @@ import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.nbt.NBTTagList;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.Icon;
+<<<<<<< HEAD
+=======
+import net.minecraft.world.IBlockAccess;
+>>>>>>> Commit
 import net.minecraft.world.World;
 import cpw.mods.fml.common.network.FMLNetworkHandler;
+import cpw.mods.fml.relauncher.Side;
+import cpw.mods.fml.relauncher.SideOnly;
 import dgrxf.watercraft.Watercraft;
 import dgrxf.watercraft.client.gui.GuiHandler;
+import dgrxf.watercraft.client.sound.Sounds;
 import dgrxf.watercraft.lib.BlockInfo;
 import dgrxf.watercraft.lib.RenderInfo;
 import dgrxf.watercraft.tileentity.WCTileEntityToolBox;
@@ -26,6 +33,7 @@ import dgrxf.watercraft.tileentity.WCTileEntityToolBox;
  */
 
 public class ToolBoxBlock extends WCBlock {
+<<<<<<< HEAD
     
     public ToolBoxBlock() {
         super(BlockInfo.TOOLBOX_ID, Material.iron);
@@ -35,6 +43,16 @@ public class ToolBoxBlock extends WCBlock {
     }
     
     @Override
+=======
+
+	public ToolBoxBlock() {
+		super(BlockInfo.TOOLBOX_ID, Material.iron);
+		setCreativeTab(Watercraft.creativeTab);
+		setUnlocalizedName(BlockInfo.TOOLBOX_UNLOCALIZED_NAME);
+	}
+	
+	@Override
+>>>>>>> Commit
     public boolean hasTileEntity(int metadata) {
         return true;
     }
@@ -51,7 +69,21 @@ public class ToolBoxBlock extends WCBlock {
         world.markBlockForUpdate(x, y, z);
     }
     
+    /**
+     * This can't access the metadata of the block, so It gets it from the tile entity
+     */
     @Override
+    public void setBlockBoundsBasedOnState(IBlockAccess world, int x, int y, int z) {
+    	WCTileEntityToolBox tile = (WCTileEntityToolBox) world.getBlockTileEntity(x, y, z);
+    	if(tile.blockMetadata == 2 || tile.blockMetadata == 3)
+    		setBlockBounds(0.1F, 0F, 0.35F, 0.9F, 0.5F, 0.65F);
+    	if(tile.blockMetadata == 4 || tile.blockMetadata == 5)
+    		setBlockBounds(0.345F, 0F, 0.1F, 0.645F, 0.5F, 0.9F);
+    }
+
+    
+    @Override
+<<<<<<< HEAD
     public boolean onBlockActivated(World world, int x, int y, int z, EntityPlayer player, int side, float hitX, float hitY, float hitZ) {
         if (world.isRemote)
             return true;
@@ -65,6 +97,20 @@ public class ToolBoxBlock extends WCBlock {
         return true;
     }
     
+=======
+	public boolean onBlockActivated(World world, int x, int y, int z, EntityPlayer player, int side, float hitX, float hitY, float hitZ) {
+		if (world.isRemote) return true;
+		
+		if(!player.isSneaking()){
+			Sounds.TOOLBOX_OPENING.play(x, y, z, 2, 1);
+			FMLNetworkHandler.openGui(player, Watercraft.instance, GuiHandler.TOOLBOX_GUI_ID, world, x, y, z); 
+		}else{
+			if(player.getCurrentEquippedItem() == null) pickUpToolBox(world, x, y, z, player);
+		}
+		return true;
+	}
+
+>>>>>>> Commit
     private void pickUpToolBox(World world, int x, int y, int z, EntityPlayer player) {
         WCTileEntityToolBox tile = (WCTileEntityToolBox) world.getBlockTileEntity(x, y, z);
         
@@ -103,6 +149,7 @@ public class ToolBoxBlock extends WCBlock {
     
     @Override
     public void breakBlock(World world, int x, int y, int z, int id, int meta) {
+<<<<<<< HEAD
         TileEntity te = world.getBlockTileEntity(x, y, z);
         if (te != null && te instanceof IInventory) {
             IInventory inventory = (IInventory) te;
@@ -128,6 +175,40 @@ public class ToolBoxBlock extends WCBlock {
             }
         }
         super.breakBlock(world, x, y, z, id, meta);
+=======
+    	TileEntity te = world.getBlockTileEntity(x, y, z);
+		if (te != null && te instanceof IInventory) {
+			IInventory inventory = (IInventory)te;
+			
+			for (int i = 0; i < inventory.getSizeInventory(); i++) {
+				ItemStack stack = inventory.getStackInSlotOnClosing(i);
+				
+				if (stack != null) {
+					float spawnX = x + world.rand.nextFloat();
+					float spawnY = y + world.rand.nextFloat();
+					float spawnZ = z + world.rand.nextFloat();
+					
+					EntityItem droppedItem = new EntityItem(world, spawnX, spawnY, spawnZ, stack);
+					
+					float mult = 0.02F;
+					
+					droppedItem.motionX = (-0.5F + world.rand.nextFloat()) * mult;
+					droppedItem.motionY = (4 + world.rand.nextFloat()) * mult;
+					droppedItem.motionZ = (-0.5F + world.rand.nextFloat()) * mult;
+					
+					world.spawnEntityInWorld(droppedItem);
+					if(i == 0)
+						world.spawnEntityInWorld(new EntityItem(world, spawnX, spawnY, spawnZ, new ItemStack(this)));
+				}
+			}
+		}
+    }
+    
+    @SideOnly(Side.CLIENT)
+    @Override
+    public Icon getIcon(int side, int meta) {
+        return Block.cloth.getIcon(0, 1);
+>>>>>>> Commit
     }
     
     @Override
