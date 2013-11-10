14578b5dfc4ccf956884cff6e8a01529ee7b32f3
TheDrunkMafia <sean.hall412@gmail.com>
2013-11-10 15:49:11 +0000

    Updated to work with open version

--- WaterCraft/src/dgrxf/watercraft/client/renderer/item/ItemToolBoxRenderer.java
+++ WaterCraft/src/dgrxf/watercraft/client/renderer/item/ItemToolBoxRenderer.java
@@ -2,9 +2,8 @@ package dgrxf.watercraft.client.renderer.item;
 
 import net.minecraft.client.Minecraft;
 import net.minecraft.item.ItemStack;
+import net.minecraft.nbt.NBTTagCompound;
 import net.minecraftforge.client.IItemRenderer;
-import net.minecraftforge.client.IItemRenderer.ItemRenderType;
-import net.minecraftforge.client.IItemRenderer.ItemRendererHelper;
 import net.minecraftforge.client.model.AdvancedModelLoader;
 import net.minecraftforge.client.model.IModelCustom;
 
@@ -14,7 +13,8 @@ import dgrxf.watercraft.lib.RenderInfo;
 
 public class ItemToolBoxRenderer implements IItemRenderer {
     
-    private IModelCustom modelBuoy = AdvancedModelLoader.loadModel("/assets/watercraft/models/Toolbox.obj");
+	private IModelCustom modelToolBox_closed = AdvancedModelLoader.loadModel("/assets/watercraft/models/toolbox.obj");
+	private IModelCustom modelToolBox_open = AdvancedModelLoader.loadModel("/assets/watercraft/models/toolbox_open.obj");
     
     @Override
     public boolean handleRenderType(ItemStack item, ItemRenderType type) {
@@ -29,6 +29,14 @@ public class ItemToolBoxRenderer implements IItemRenderer {
     @Override
     public void renderItem(ItemRenderType type, ItemStack item, Object... data) {
         GL11.glPushMatrix();
+        NBTTagCompound tag;
+        boolean isOpen = false;
+        
+        if(item.getTagCompound() != null){
+        	tag = item.getTagCompound();
+        	isOpen = tag.getBoolean("isOpen");
+        }
+        
         Minecraft.getMinecraft().renderEngine.bindTexture(RenderInfo.TOOLBOX_TEXTURE_LOCATION);
         switch (type) {
             case EQUIPPED:
@@ -41,16 +49,22 @@ public class ItemToolBoxRenderer implements IItemRenderer {
                 GL11.glScalef(1.5F, 1.5F, 1.5F);
                 break;
             case INVENTORY:
-                GL11.glTranslatef(0.0F, 0.3F, 0.0F);
-                GL11.glRotatef(180, 0F, 1F, 0);
-                GL11.glScalef(1.8F, 1.8F, 1.8F);
+            	if(isOpen){
+            		GL11.glTranslatef(-0.2F, 0.1F, 0.0F);
+                    GL11.glScalef(1.6F, 1.6F, 1.6F);
+            	}else{
+            		GL11.glTranslatef(0.0F, 0.3F, 0.0F);
+            		GL11.glScalef(1.8F, 1.8F, 1.8F);
+            	}
+	                GL11.glRotatef(180, 0F, 1F, 0);
                 break;
             default:
         }
         
-        GL11.glScalef(1F, 1F, 1F);
-        
-        modelBuoy.renderAll();
+        if(isOpen)
+        	modelToolBox_open.renderAll();
+        else
+        	modelToolBox_closed.renderAll();
         
         GL11.glPopMatrix();
     }
