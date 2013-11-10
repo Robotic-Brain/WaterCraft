6ba050f02b634e27d8e36d4920ea014b1859af31
FrodCube <frodcube@gmail.com>
2013-11-10 11:58:56 +0100

    Changes in Vector2 class

--- WaterCraft/src/dgrxf/watercraft/util/Vector2.java
+++ WaterCraft/src/dgrxf/watercraft/util/Vector2.java
@@ -1,10 +1,9 @@
 package dgrxf.watercraft.util;
 
 public class Vector2 {
-    public float          x;
-    public float          z;
+	public float          x, z;
     
-    public static Vector2 ZERO = new Vector2(0, 0);
+    public final static Vector2 ZERO = new Vector2(0, 0);
     
     public Vector2(float x, float z) {
         this.x = x;
@@ -13,7 +12,32 @@ public class Vector2 {
     
     @Override
     public String toString() {
-        return "{ " + x + ", " + z + " }";
+        return "( " + x + ", " + z + " )";
     }
+    
+    public static Vector2 add(Vector2 v1, Vector2 v2) {
+    	return new Vector2(v1.x + v2.x, v1.z + v2.z);
+    }
+    
+    public static Vector2 scalarMult(Vector2 v, float a) {
+    	return new Vector2(a * v.x, a * v.z);
+    }
+    
+    public static Vector2 subtract(Vector2 v1, Vector2 v2) {
+    	return add(v1, scalarMult(v2, -1));
+    }
+    
+    public static float dotProduct(Vector2 v1, Vector2 v2) {
+    	return v1.x * v2.x + v1.z * v2.z;
+    }
+    
+    public static float squareLength(Vector2 v) {
+    	return dotProduct(v, v);
+    }
+    
+    public static float length(Vector2 v) {
+    	return (float) Math.sqrt(squareLength(v));
+    }
+    
     
 }
