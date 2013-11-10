ca136ebe7197bdc1b67050c359dc80aa5521e19e
FrodCube <frodcube@gmail.com>
2013-11-10 20:21:45 +0100

    a little change in the particles

--- WaterCraft/src/dgrxf/watercraft/client/particles/BuoyParticle.java
+++ WaterCraft/src/dgrxf/watercraft/client/particles/BuoyParticle.java
@@ -6,12 +6,22 @@ import net.minecraft.world.World;
 public class BuoyParticle extends EntityFX {
 
 	public BuoyParticle(World world, double x, double y, double z, double vx, double vy, double vz) {
-		super(world, x, y, z, vx, vy, vz);
-		//particleAge = 100000;
+		super(world, x, y, z, 0, 0, 0);
+		motionX = vx;
+		motionY = vy;
+		motionZ = vz;
+		particleMaxAge = 100000;
+		particleGravity = 0.1F;
 		
 		//TODO particle age
 		//func_110125_a(NEEDS AN ICON);
 
+	}
+	
+	@Override
+	public void onEntityUpdate() {
+		super.onEntityUpdate();
+		
 	}
 	
 	@Override
