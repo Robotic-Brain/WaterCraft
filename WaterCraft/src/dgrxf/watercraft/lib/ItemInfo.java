0d9ddfb8b4867a4d359bac0f707b0b2de9ad8e40
TheDrunkMafia <sean.hall412@gmail.com>
2013-11-10 15:47:11 +0000

    Updated Entries

--- WaterCraft/src/dgrxf/watercraft/lib/ItemInfo.java
+++ WaterCraft/src/dgrxf/watercraft/lib/ItemInfo.java
@@ -3,7 +3,7 @@ package dgrxf.watercraft.lib;
 /**
  * Class Made By: ???
  * 
- * Class Last Edited By: xandayn Class Last Edited On: 07/11/2013 DD/MM/YYYY
+ * Class Last Edited By: Drunk Mafia Class Last Edited On: 10/11/2013 DD/MM/YYYY
  * 
  */
 
@@ -14,14 +14,35 @@ public class ItemInfo {
      *************/
     
     public static final String BOAT_UNLOCALIZED_NAME = "wcboat";
+<<<<<<< HEAD
     // public static final String boat_LocalizedName = "WC Boat"; Don't need
     // localized names, this is done in the .lang files
     public static final String BOAT_KEY              = "wc boat";
+=======
+    public static final String BOAT_KEY = "wc boat";
+>>>>>>> Commit
+    
+    public static final String TAPE_MEASURE_UNLOCALIZED_NAME = "TapeItemName";
+    public static final String TAPE_MEASURE_KEY = "Tape Measure";
+    
+    public static final String TELESCOPE_UNLOCALIZED_NAME = "TelescopeName";
+    public static final String TELESCOPE_KEY = "Telescope";
     
     /*************
      * Integers
      *************/
     
+<<<<<<< HEAD
     public static final int    BOAT_ID_DEFAULT       = 700;
     public static int          BOAT_ID;
+=======
+    public static final int BOAT_ID_DEFAULT = 700;
+    public static int BOAT_ID;
+    
+    public static final int TAPE_MEASURE_ID_DEFAULT = 707;
+    public static int TAPE_MEASURE_ID;
+    
+    public static final int TELESCOPE_ID_DEFAULT = 708;
+    public static int TELESCOPE_ID;
+>>>>>>> Commit
 }
