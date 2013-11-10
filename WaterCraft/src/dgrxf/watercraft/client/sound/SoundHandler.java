c261ac16207c50b3cede8bb2bbc6f3d070e0e65e
TheDrunkMafia <sean.hall412@gmail.com>
2013-11-10 15:48:08 +0000

    Robbed code from VSWE -.-

---
+++ WaterCraft/src/dgrxf/watercraft/client/sound/SoundHandler.java
@@ -0,0 +1,31 @@
+package dgrxf.watercraft.client.sound;
+
+import net.minecraftforge.client.event.sound.SoundLoadEvent;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.event.ForgeSubscribe;
+import cpw.mods.fml.relauncher.Side;
+import cpw.mods.fml.relauncher.SideOnly;
+
+/**
+ * (Original from VSWE Courses)
+ */
+
+@SideOnly(Side.CLIENT)
+public class SoundHandler {
+
+	public SoundHandler() {
+		MinecraftForge.EVENT_BUS.register(this);
+	}
+	
+	@ForgeSubscribe
+	public void onSoundsLoad(SoundLoadEvent event) {
+		for (Sounds sound : Sounds.values()) {
+			addSound(event, sound);
+		}
+	}
+	
+	private void addSound(SoundLoadEvent event, Sounds sound) {
+		event.manager.soundPoolSounds.addSound(Sounds.SOUNDS_LOCATION + ":" + sound.getName() + ".ogg");
+	}
+	
+}
