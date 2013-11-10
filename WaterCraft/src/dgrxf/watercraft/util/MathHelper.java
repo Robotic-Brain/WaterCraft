307d3b5beb0bb32427eea27b067801168263073a
FrodCube <frodcube@gmail.com>
2013-11-10 18:09:57 +0100

    Math fix

--- WaterCraft/src/dgrxf/watercraft/util/MathHelper.java
+++ WaterCraft/src/dgrxf/watercraft/util/MathHelper.java
@@ -22,42 +22,9 @@ public class MathHelper {
         return (float) Math.sqrt(calculateVector2SquareDistance(positionOne, positionTwo));
     }
     
-    /*
-     * can't you just do this? 
-     * public static float calculatePointDistance(float positionOne, float positionTwo) {
-     * 	  return Math.abs(positionOne - positionTwo);
-     * }
-     * 
-     * - Frod
-     * 
-     * No, because if Im going from -700 to 700 Math.abs(-700 - 700) = 0 because the absolute value of -700 is 700, so I'm shifting both values by the smaller so
-     * I get 0 and 1400 so then 1400 - 0 = 0, which is the correct distance.
-     * 
-     * -xandayn
-     * 
-     * - 700 - 700 = - 1400
-     * Math.abs(-700 - 700) = 1400
-     * 
-     * - Frod
-     * 
-     * Yeah you're right LOL I totally derped that up xD Shhhh no one needs to know! I like overcomplicating things >.>
-     * 
-     */
-    
     public static float calculatePointDistance(float positionOne, float positionTwo) {
-        float tempDistOne, tempDistTwo, distance = 0;
-        if (positionOne > positionTwo) {
-            float tempSmaller = Math.abs(positionTwo) + positionTwo;
-            float tempLarger = Math.abs(positionTwo) + positionOne;
-            distance = tempLarger - tempSmaller;
-        } else {
-            float tempSmaller = Math.abs(positionOne) + positionOne;
-            float tempLarger = Math.abs(positionOne) + positionTwo;
-            distance = tempLarger - tempSmaller;
+         	  return Math.abs(positionOne - positionTwo);
         }
-        
-        return distance;
-    }
     
     /**
      * Clamps a value within range
