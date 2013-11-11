a53575b1d24104ec3e64a1afc14673bc8db1f7d7
TheDrunkMafia <sean.hall412@gmail.com>
2013-11-11 17:30:22 +0000

    Created the base of the item flag

---
+++ WaterCraft/src/dgrxf/watercraft/item/ItemFlag.java
@@ -0,0 +1,13 @@
+package dgrxf.watercraft.item;
+
+import dgrxf.watercraft.Watercraft;
+import dgrxf.watercraft.lib.ItemInfo;
+import net.minecraft.item.Item;
+
+public class ItemFlag extends Item{
+	public ItemFlag() {
+		super(ItemInfo.FLAG_ID);
+		setUnlocalizedName(ItemInfo.FLAG_UNLOCALIZED_NAME);
+		setCreativeTab(Watercraft.creativeTab);
+	}
+}
