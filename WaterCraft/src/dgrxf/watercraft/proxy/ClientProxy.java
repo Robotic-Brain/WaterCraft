df15c57f2605efaa0b682d898a9b6587285ea582
TheDrunkMafia <sean.hall412@gmail.com>
2013-11-12 17:53:41 +0000

    Trying to bind the filters renderer

--- WaterCraft/src/dgrxf/watercraft/proxy/ClientProxy.java
+++ WaterCraft/src/dgrxf/watercraft/proxy/ClientProxy.java
@@ -3,24 +3,24 @@ package dgrxf.watercraft.proxy;
 import net.minecraftforge.client.MinecraftForgeClient;
 import cpw.mods.fml.client.registry.ClientRegistry;
 import cpw.mods.fml.client.registry.RenderingRegistry;
+import dgrxf.watercraft.client.renderer.block.BuoyFilterRenderer;
 import dgrxf.watercraft.client.renderer.block.BuoyRenderer;
 import dgrxf.watercraft.client.renderer.block.ControlUnitRenderer;
 import dgrxf.watercraft.client.renderer.block.ToolBoxRenderer;
 import dgrxf.watercraft.client.renderer.entity.WCBoatRenderer;
 import dgrxf.watercraft.client.renderer.item.ItemBuoyRenderer;
 import dgrxf.watercraft.client.renderer.item.ItemControlUnitRenderer;
-import dgrxf.watercraft.client.renderer.item.ItemFlagRenderer;
 import dgrxf.watercraft.client.renderer.item.ItemToolBoxRenderer;
 import dgrxf.watercraft.client.sound.SoundHandler;
 import dgrxf.watercraft.entity.WCEntityBoat;
 import dgrxf.watercraft.entity.WCEntityBoatBase;
 import dgrxf.watercraft.entity.WCEntitySmartBoat;
 import dgrxf.watercraft.lib.BlockInfo;
-import dgrxf.watercraft.lib.ItemInfo;
 import dgrxf.watercraft.lib.RenderInfo;
 import dgrxf.watercraft.tileentity.WCTileEntityControlUnitDock;
 import dgrxf.watercraft.tileentity.WCTileEntityToolBox;
 import dgrxf.watercraft.tileentity.buoy.WCTileEntityBuoy;
+import dgrxf.watercraft.tileentity.buoy.WCTileEntityFilterBuoy;
 
 /**
  * Client Proxy
@@ -37,6 +37,7 @@ public class ClientProxy extends CommonProxy {
         RenderInfo.BUOY_RENDER_ID = RenderingRegistry.getNextAvailableRenderId();
         RenderInfo.CONTROL_UNIT_RENDER_ID = RenderingRegistry.getNextAvailableRenderId();
         RenderInfo.TOOLBOX_RENDER_ID = RenderingRegistry.getNextAvailableRenderId();
+        RenderInfo.BUOY_FILTER_RENDER_ID = RenderingRegistry.getNextAvailableRenderId();
         
         ClientRegistry.bindTileEntitySpecialRenderer(WCTileEntityBuoy.class, new BuoyRenderer());
         ClientRegistry.bindTileEntitySpecialRenderer(WCTileEntityControlUnitDock.class, new ControlUnitRenderer());
@@ -47,8 +48,8 @@ public class ClientProxy extends CommonProxy {
         RenderingRegistry.registerEntityRenderingHandler(WCEntitySmartBoat.class, new WCBoatRenderer());
         
         MinecraftForgeClient.registerItemRenderer(BlockInfo.BUOY_ID, new ItemBuoyRenderer());
+        MinecraftForgeClient.registerItemRenderer(BlockInfo.BUOY_FILTER_ID, new ItemBuoyRenderer());
         MinecraftForgeClient.registerItemRenderer(BlockInfo.TOOLBOX_ID, new ItemToolBoxRenderer());
         MinecraftForgeClient.registerItemRenderer(BlockInfo.CONTROL_UNIT_DOCK_ID, new ItemControlUnitRenderer());
-        MinecraftForgeClient.registerItemRenderer(ItemInfo.FLAG_ID, new ItemFlagRenderer());
     }
 }
