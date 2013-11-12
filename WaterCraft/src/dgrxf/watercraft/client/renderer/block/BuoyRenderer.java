71af9217b4ad6cdebca8d4c33d09cd0274215e9d
TheDrunkMafia <sean.hall412@gmail.com>
2013-11-12 17:54:23 +0000

    Removed old animation code

--- WaterCraft/src/dgrxf/watercraft/client/renderer/block/BuoyRenderer.java
+++ WaterCraft/src/dgrxf/watercraft/client/renderer/block/BuoyRenderer.java
@@ -9,6 +9,7 @@ import net.minecraftforge.client.model.IModelCustom;
 import org.lwjgl.opengl.GL11;
 
 import dgrxf.watercraft.lib.RenderInfo;
+import dgrxf.watercraft.tileentity.buoy.WCTileEntityFilterBuoy;
 
 /**
  * Class Made By: Drunk Mafia
@@ -27,14 +28,14 @@ public class BuoyRenderer extends TileEntitySpecialRenderer {
     
     @Override
     public void renderTileEntityAt(TileEntity tileentity, double x, double y, double z, float partialTickTime) {
-        GL11.glPushMatrix();
-        
-        // If this would be used, maybe control it from the TileEntity
-        /*
-         * tickTick++; if(tickTick % 4 == 0) tick++; if(up){ yLevel += 0.01F;
-         * if(tick > 16){ up = false; } }else if(!up){ yLevel -= 0.01F; if(tick
-         * > 32){ up = true; tick = -1; yLevel = 0; tickTick = 0; tick = 0; } }
-         */
+    	renderBouy(x, y, z);
+    	if(tileentity instanceof WCTileEntityFilterBuoy){
+    		System.out.println("HELLO!");
+    	}
+    }
+    
+    public void renderBouy(double x, double y, double z){
+    	GL11.glPushMatrix();
         
         GL11.glTranslatef((float) x + 0.5F, (float) y - 0.5F + yLevel, (float) z + 0.5F);
         Minecraft.getMinecraft().renderEngine.bindTexture(RenderInfo.BUOY_TEXTURE_LOCATION);
@@ -42,5 +43,9 @@ public class BuoyRenderer extends TileEntitySpecialRenderer {
         modelBuoy.renderAll();
         
         GL11.glPopMatrix();
+    }
+    
+    public void renderFlags(WCTileEntityFilterBuoy tile, double x, double y, double z){
+    	System.out.println("This is a filter");
     }
 }
