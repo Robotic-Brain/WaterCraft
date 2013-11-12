fd7f3bea86765d05a6843659951a63adaa5f9912
TheDrunkMafia <sean.hall412@gmail.com>
2013-11-12 23:05:45 +0000

    Finally fixed my derp

--- WaterCraft/src/dgrxf/watercraft/tileentity/buoy/WCTileEntityFilterBuoy.java
+++ WaterCraft/src/dgrxf/watercraft/tileentity/buoy/WCTileEntityFilterBuoy.java
@@ -12,20 +12,23 @@ import dgrxf.watercraft.enumeration.Colours;
 public class WCTileEntityFilterBuoy extends WCBouyLogic {
 	
 	public Colours[] directions;
+	public int tick;
 	
 	public WCTileEntityFilterBuoy() {
 		directions = new Colours[4];
+		tick = 30;
 	}	
 	
 	@Override
 	public void updateEntity() {		
 		  if(worldObj.isRemote) return;
 		
-		  WCEntityBoat e = findEntityBoat(getBlockDirection(), WCEntityBoat.class);
-		  
-		  if(e != null){
-			  if(e.flag != null)
-				  System.out.println(e.flag.name);
+		  tick--;
+		  if(tick <= 0){
+			  WCEntityBoat e = findEntityBoat(getBlockDirection(), WCEntityBoat.class);
+			  if(e != null){
+				  
+			  }
 		  }
 	}
 	
