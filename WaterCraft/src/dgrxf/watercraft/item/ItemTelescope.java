4aa357fca3db8ba65c2baf83f26306389313b417
FrodCube <frodcube@gmail.com>
2013-11-12 14:03:05 +0100

    Added temp telescope texture

--- WaterCraft/src/dgrxf/watercraft/item/ItemTelescope.java
+++ WaterCraft/src/dgrxf/watercraft/item/ItemTelescope.java
@@ -1,5 +1,8 @@
 package dgrxf.watercraft.item;
 
+import cpw.mods.fml.relauncher.Side;
+import cpw.mods.fml.relauncher.SideOnly;
+import net.minecraft.client.renderer.texture.IconRegister;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
@@ -49,6 +52,12 @@ public class ItemTelescope extends Item{
         }
         
         return false;
+    }
+    
+    @Override
+    @SideOnly(Side.CLIENT)
+    public void registerIcons(IconRegister register) {
+    	itemIcon = register.registerIcon("Watercraft:telescope");
     }
 
 }
