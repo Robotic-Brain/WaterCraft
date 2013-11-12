275eea0437908c939c05fce593743681928fcd87
TheDrunkMafia <sean.hall412@gmail.com>
2013-11-12 17:11:06 +0000

    Added filter

--- WaterCraft/src/dgrxf/watercraft/block/ModBlocks.java
+++ WaterCraft/src/dgrxf/watercraft/block/ModBlocks.java
@@ -2,13 +2,15 @@ package dgrxf.watercraft.block;
 
 import net.minecraft.block.Block;
 import cpw.mods.fml.common.registry.GameRegistry;
+import dgrxf.watercraft.block.buoy.BuoyBlock;
+import dgrxf.watercraft.block.buoy.BuoyFilterBlock;
 import dgrxf.watercraft.item.ItemBlockBuoy;
 import dgrxf.watercraft.item.ItemBlockToolBox;
 import dgrxf.watercraft.lib.BlockInfo;
-import dgrxf.watercraft.tileentity.WCTileEntityBuoy;
 import dgrxf.watercraft.tileentity.WCTileEntityControlUnitDock;
 import dgrxf.watercraft.tileentity.WCTileEntityFreezer;
 import dgrxf.watercraft.tileentity.WCTileEntityToolBox;
+import dgrxf.watercraft.tileentity.buoy.WCTileEntityBuoy;
 
 /**
  * Mod Blocks
@@ -18,20 +20,26 @@ import dgrxf.watercraft.tileentity.WCTileEntityToolBox;
  */
 public class ModBlocks {
     
-    public static Block buoy;
+    public static BuoyBlock buoy;
+    public static BuoyBlock filter;
     public static Block controlUnitDock;
     public static Block freezer;
     public static Block dropZone;
     public static Block toolbox;
     
     public static void init() {
+    	//Buoy Blocks
         buoy = new BuoyBlock(BlockInfo.BUOY_ID);
+        filter = new BuoyFilterBlock(BlockInfo.BUOY_FILTER_ID);
+        
         controlUnitDock = new ControlBlockDock(BlockInfo.CONTROL_UNIT_DOCK_ID);
         freezer = new WaterFreezerBlock(BlockInfo.FREEZER_ID);
         dropZone = new DropZoneBlock();
         toolbox = new ToolBoxBlock();
         
         GameRegistry.registerBlock(buoy, ItemBlockBuoy.class, BlockInfo.BUOY_UNLOCALIZED_NAME);
+        GameRegistry.registerBlock(filter, ItemBlockBuoy.class, BlockInfo.BUOY_FILTER_UNLOCALIZED_NAME);
+        
         GameRegistry.registerBlock(controlUnitDock, BlockInfo.CONTROL_UNIT_DOCK_UNLOCALIZED_NAME);
         GameRegistry.registerBlock(freezer, BlockInfo.FREEZER_UNLOCALIZED_NAME);
         GameRegistry.registerBlock(toolbox, ItemBlockToolBox.class, BlockInfo.TOOLBOX_UNLOCALIZED_NAME);
