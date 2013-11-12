8df691e733b1e29e9e46cd63df91f91cc94efad2
xandayn <xandayn101@hotmail.com>
2013-11-12 16:55:17 -0700

    Made this a bit smarter, removed an unneeded int

--- WaterCraft/src/dgrxf/watercraft/client/gui/GuiColor.java
+++ WaterCraft/src/dgrxf/watercraft/client/gui/GuiColor.java
@@ -1,33 +1,31 @@
 package dgrxf.watercraft.client.gui;
 
 public enum GuiColor {
-    BLACK(0, 0x000000),
-    BLUE(1, 0x0000AA),
-    GREEN(2, 0x00AA00),
-    CYAN(3, 0x00AAAA),
-    RED(4, 0xAA0000),
-    PURPLE(5, 0xAA00AA),
-    ORANGE(6, 0xFFAA00),
-    LIGHTGRAY(7, 0xAAAAAA),
-    GRAY(8, 0x555555),
-    LIGHTBLUE(9, 0x5555FF),
-    LIME(10, 0x55FF55),
-    TURQUISE(11, 0x55FFFF),
-    PINK(12, 0xFF5555),
-    MAGNETA(13, 0xFF55FF),
-    YELLOW(14, 0xFFFF55),
-    WHITE(15, 0xFFFFFF);
+    BLACK(0x000000),
+    BLUE(0x0000AA),
+    GREEN(0x00AA00),
+    CYAN(0x00AAAA),
+    RED(0xAA0000),
+    PURPLE(0xAA00AA),
+    ORANGE(0xFFAA00),
+    LIGHTGRAY(0xAAAAAA),
+    GRAY(0x555555),
+    LIGHTBLUE(0x5555FF),
+    LIME(0x55FF55),
+    TURQUISE(0x55FFFF),
+    PINK(0xFF5555),
+    MAGNETA(0xFF55FF),
+    YELLOW(0xFFFF55),
+    WHITE(0xFFFFFF);
     
-    private int number;
     private int rgb;
-    private GuiColor(int number, int rgb) {
-        this.number = number;
+    private GuiColor(int rgb) {
         this.rgb = rgb;
     }
     
     @Override
     public String toString() {
-        return "\u00a7" + Integer.toHexString(number);
+        return "\u00a7" + Integer.toHexString(ordinal());
     }
     
     public int toRGB() {
