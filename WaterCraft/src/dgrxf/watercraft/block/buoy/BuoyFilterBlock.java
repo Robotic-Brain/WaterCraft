992e07060c35c9dd37b855a271b8504467731296
TheDrunkMafia <sean.hall412@gmail.com>
2013-11-12 17:27:49 +0000

    Adds flags to filter

--- WaterCraft/src/dgrxf/watercraft/block/buoy/BuoyFilterBlock.java
+++ WaterCraft/src/dgrxf/watercraft/block/buoy/BuoyFilterBlock.java
@@ -5,6 +5,8 @@ import java.util.List;
 import net.minecraft.block.Block;
 import net.minecraft.block.material.Material;
 import net.minecraft.entity.Entity;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.item.ItemStack;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.AxisAlignedBB;
 import net.minecraft.util.Icon;
@@ -14,7 +16,9 @@ import cpw.mods.fml.relauncher.SideOnly;
 import dgrxf.watercraft.Watercraft;
 import dgrxf.watercraft.block.DirectionalBlock;
 import dgrxf.watercraft.entity.WCEntityBoat;
+import dgrxf.watercraft.enumeration.Colours;
 import dgrxf.watercraft.lib.BlockInfo;
+import dgrxf.watercraft.lib.ItemInfo;
 import dgrxf.watercraft.lib.RenderInfo;
 import dgrxf.watercraft.tileentity.buoy.WCTileEntityFilterBuoy;
 
@@ -22,10 +26,23 @@ public class BuoyFilterBlock extends BuoyBlock {
     
     public BuoyFilterBlock(int id) {
         super(id);
-        setCreativeTab(Watercraft.creativeTab);
-        setUnlocalizedName(BlockInfo.BUOY_UNLOCALIZED_NAME + "Filter");
-        setBlockBounds(0.1F, -1.3F, 0.1F, 0.9F, 0.9F, 0.9F);
-        setLightValue(1F);
+        setUnlocalizedName(BlockInfo.BUOY_FILTER_UNLOCALIZED_NAME);
+    }
+    
+    @Override
+    public boolean onBlockActivated(World world, int x, int y, int z, EntityPlayer player, int side, float hitX, float hitY, float hitZ) {
+    	if(world.isRemote)
+    		return false;
+    	
+    	ItemStack stack = player.getCurrentEquippedItem();
+    	WCTileEntityFilterBuoy tile = (WCTileEntityFilterBuoy) world.getBlockTileEntity(x, y, z);
+    	
+    	if(tile != null && stack != null && stack.getItem().itemID == ItemInfo.FLAG_ID){
+    		Colours[] temp = Colours.values();
+    		tile.setColour(side, temp[stack.getItemDamage()]);
+    		return true;
+    	}
+    	return false;
     }
     
     @Override
