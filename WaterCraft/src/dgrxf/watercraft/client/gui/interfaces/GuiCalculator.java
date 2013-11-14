199d6dd7c441307c7183b2a074762c555d3f4c8e
TheDrunkMafia <sean.hall412@gmail.com>
2013-11-14 21:57:10 +0000

    Removed test code

--- WaterCraft/src/dgrxf/watercraft/client/gui/interfaces/GuiCalculator.java
+++ WaterCraft/src/dgrxf/watercraft/client/gui/interfaces/GuiCalculator.java
@@ -1,5 +1,7 @@
 package dgrxf.watercraft.client.gui.interfaces;
 
+import java.util.ArrayList;
+
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.GuiButton;
 import net.minecraft.client.gui.inventory.GuiContainer;
@@ -12,11 +14,11 @@ import dgrxf.watercraft.client.gui.container.CalculatorContainer;
 
 public class GuiCalculator extends GuiContainer{
 
-	private String screenText = "0";
+	private String screenText;
+	private String temp;
 	
 	public GuiCalculator() {
-		super(new CalculatorContainer());
-		
+		super(new CalculatorContainer());	
         xSize = 196;
         ySize = 218;
 	}
@@ -33,8 +35,8 @@ public class GuiCalculator extends GuiContainer{
 	@Override
 	protected void drawGuiContainerForegroundLayer(int x, int y) {
 		fontRenderer.drawString("Calculator", 8, 3, 0x404040);
-		fontRenderer.drawSplitString(screenText, 10, 17, 178, 0x404040);
 		
+		fontRenderer.drawSplitString(screenText, 10, 17, 178, 0x404040);
 	}
 	
 	@Override
@@ -54,13 +56,13 @@ public class GuiCalculator extends GuiContainer{
 		buttonList.add(new GuiButton((id + 2), guiLeft + 31, 118, 18, 20, "-"));
 		buttonList.add(new GuiButton((id + 3), guiLeft + 50, 118, 18, 20, "/"));
 		buttonList.add(new GuiButton((id + 2), guiLeft + 69, 118, 18, 20, "*"));
-		buttonList.add(new GuiButton((id + 2), guiLeft + 12, 96, 57, 20, "="));
-		buttonList.add(new GuiButton((id + 2), guiLeft + 88, 118, 18, 20, "C"));
-		buttonList.add(new GuiButton((id + 2), guiLeft + 69, 96, 38, 20, "<="));
+		buttonList.add(new GuiButton((id + 3), guiLeft + 12, 96, 57, 20, "="));
+		buttonList.add(new GuiButton((id + 4), guiLeft + 88, 118, 18, 20, "C"));
+		buttonList.add(new GuiButton((id + 5), guiLeft + 69, 96, 38, 20, "<="));
 	}
 	
 	@Override
 	protected void actionPerformed(GuiButton button) {
-		screenText += button.id;
+		
 	}
 }
