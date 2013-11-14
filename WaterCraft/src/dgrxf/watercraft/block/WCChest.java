69aca117eb4902f8e978a67db43d04d277865abe
FrodCube <frodcube@gmail.com>
2013-11-14 18:46:22 +0100

    nothing changed

--- WaterCraft/src/dgrxf/watercraft/block/WCChest.java
+++ WaterCraft/src/dgrxf/watercraft/block/WCChest.java
@@ -9,7 +9,6 @@ import net.minecraft.block.Block;
 import net.minecraft.block.BlockContainer;
 import net.minecraft.block.material.Material;
 import net.minecraft.client.renderer.texture.IconRegister;
-import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.item.EntityItem;
 import net.minecraft.entity.passive.EntityOcelot;
