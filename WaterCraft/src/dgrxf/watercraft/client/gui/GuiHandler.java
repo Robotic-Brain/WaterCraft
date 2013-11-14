ec00c589bae5b186dc370223aed2b572cc337ed5
TheDrunkMafia <sean.hall412@gmail.com>
2013-11-14 21:25:38 +0000

    Added some locals

--- WaterCraft/src/dgrxf/watercraft/client/gui/GuiHandler.java
+++ WaterCraft/src/dgrxf/watercraft/client/gui/GuiHandler.java
@@ -5,18 +5,23 @@ import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.world.World;
 import cpw.mods.fml.common.network.IGuiHandler;
-import dgrxf.watercraft.client.gui.container.ToolboxContainer;
-import dgrxf.watercraft.client.gui.interfaces.ToolBoxGUI;
-import dgrxf.watercraft.lib.BlockInfo;
-import dgrxf.watercraft.tileentity.WCTileEntityToolBox;
+import dgrxf.watercraft.client.gui.container.CalculatorContainer;
 import dgrxf.watercraft.client.gui.container.ControlUnitContainer;
+import dgrxf.watercraft.client.gui.container.ToolboxContainer;
+import dgrxf.watercraft.client.gui.interfaces.GuiCalculator;
+import dgrxf.watercraft.client.gui.interfaces.ToolBoxGUI;
 import dgrxf.watercraft.client.gui.interfaces.controlunit.ControlUnitGUI;
+import dgrxf.watercraft.item.ModItems;
+import dgrxf.watercraft.lib.BlockInfo;
+import dgrxf.watercraft.lib.ItemInfo;
 import dgrxf.watercraft.tileentity.WCTileEntityControlUnitDock;
+import dgrxf.watercraft.tileentity.WCTileEntityToolBox;
 
 public class GuiHandler implements IGuiHandler {
     
     public static final int TOOLBOX_GUI_ID = 0;
     public static final int CONTROLUNIT_GUI_ID = 1;
+    public static final int CALCULATOR_GUI_ID = 2;
     
     public GuiHandler() {
     }
@@ -37,6 +42,11 @@ public class GuiHandler implements IGuiHandler {
             		return new ControlUnitContainer(player.inventory, (WCTileEntityControlUnitDock)te);
             	}
             	break;
+            case CALCULATOR_GUI_ID:
+            	if(player.inventory.getCurrentItem().getItem().itemID == ModItems.calculator.itemID){
+            		return new CalculatorContainer();
+            	}else System.out.println("Item is not valid");
+            	break;
         }
         return null;
     }
@@ -56,6 +66,11 @@ public class GuiHandler implements IGuiHandler {
             	if (te instanceof WCTileEntityControlUnitDock) {
             		return new ControlUnitGUI(player.inventory, (WCTileEntityControlUnitDock)te);
             	}
+            	break;
+            case CALCULATOR_GUI_ID:
+            	if(player.inventory.getCurrentItem().getItem().itemID == ModItems.calculator.itemID){
+            		return new GuiCalculator();
+            	}else System.out.println("Item is not valid");
             	break;
         }
         return null;
