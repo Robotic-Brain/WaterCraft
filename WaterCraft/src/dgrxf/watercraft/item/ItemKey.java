92ba81586a46372763df7bd16328682038d0d76b
FrodCube <frodcube@gmail.com>
2013-11-15 19:08:07 +0100

    Key and locks now interact

--- WaterCraft/src/dgrxf/watercraft/item/ItemKey.java
+++ WaterCraft/src/dgrxf/watercraft/item/ItemKey.java
@@ -1,8 +1,14 @@
 package dgrxf.watercraft.item;
 
-import dgrxf.watercraft.Watercraft;
-import dgrxf.watercraft.lib.ItemInfo;
+import net.minecraft.entity.item.EntityItem;
+import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.tileentity.TileEntity;
+import net.minecraft.world.World;
+import dgrxf.watercraft.Watercraft;
+import dgrxf.watercraft.interfaces.ILockableBlock;
+import dgrxf.watercraft.lib.ItemInfo;
 
 public class ItemKey extends Item {
 
@@ -12,6 +18,28 @@ public class ItemKey extends Item {
         setCreativeTab(Watercraft.miscTab);
 	}
 	
-	
+	@Override
+	public boolean onItemUseFirst(ItemStack stack, EntityPlayer player, World world, int x, int y, int z, int side, float hitX, float hitY, float hitZ) {
+		if (!world.isRemote) {
+			if (player.isSneaking()) {
+				TileEntity te = world.getBlockTileEntity(x, y, z);
+				
+				if (te instanceof ILockableBlock) {
+					int code = ((ILockableBlock)te).getCode();
+					
+					if (code == stack.getItemDamage()) {
+						 ((ILockableBlock)te).setLocked(false);
+						 ((ILockableBlock)te).setCode(-1);
+						 world.markBlockForUpdate(x, y, z);
 
+						 EntityItem entityitem = new EntityItem(world, x, y + 1, z, new ItemStack(ModItems.padlock, 1, code));
+				         entityitem.delayBeforeCanPickup = 10;
+				         world.spawnEntityInWorld(entityitem);
+					}
+				}
+			}
+		}
+		
+		return true;
+	}
 }
