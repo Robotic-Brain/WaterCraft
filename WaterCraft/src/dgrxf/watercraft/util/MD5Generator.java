65b48a7797541c24028b702bc0814dc6ce042e2c
xandayn <xandayn101@hotmail.com>
2013-11-15 14:43:07 -0700

    Added a hash generator for key/locks, not needed but it seemed like it'd be fun

---
+++ WaterCraft/src/dgrxf/watercraft/util/MD5Generator.java
@@ -0,0 +1,22 @@
+package dgrxf.watercraft.util;
+
+import java.math.BigInteger;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+public class MD5Generator {
+	
+	public static String generateHash(String toHash) {
+		BigInteger i = null;
+		try{
+			String returnHash = "";
+			MessageDigest m = MessageDigest.getInstance("MD5");
+			byte[] data = toHash.getBytes();
+			m.update(data, 0, data.length);
+			i = new BigInteger(1, m.digest());
+		}catch(NoSuchAlgorithmException e){
+			e.printStackTrace();
+		}
+		return String.format("%1$032X", i);
+	}
+}
