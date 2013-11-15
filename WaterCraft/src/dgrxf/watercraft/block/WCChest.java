12699db26591b31e2d918241abd62ebf5c78c3be
xandayn <xandayn101@hotmail.com>
2013-11-15 10:49:53 -0700

    Fixed block set and step sounds

--- WaterCraft/src/dgrxf/watercraft/block/WCChest.java
+++ WaterCraft/src/dgrxf/watercraft/block/WCChest.java
@@ -43,6 +43,7 @@ public class WCChest extends BlockContainer {
 	        this.setCreativeTab(Watercraft.miscTab);
 	        this.setUnlocalizedName(BlockInfo.WC_CHEST_UNLOCALIZED_NAME);
 	        this.setBlockBounds(0.0625F, 0.0F, 0.0625F, 0.9375F, 0.875F, 0.9375F);
+	        this.setStepSound(soundWoodFootstep);
 	    }
 	    
 	    public boolean isOpaqueCube() {
