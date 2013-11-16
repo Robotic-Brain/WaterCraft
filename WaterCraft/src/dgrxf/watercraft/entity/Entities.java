2c2c564764204d2ea84b46eb075dfb48e64e1e4e
xandayn <xandayn101@hotmail.com>
2013-11-16 16:05:39 -0700

    Fixed crashing from placing boat

--- WaterCraft/src/dgrxf/watercraft/entity/Entities.java
+++ WaterCraft/src/dgrxf/watercraft/entity/Entities.java
@@ -3,7 +3,7 @@ package dgrxf.watercraft.entity;
 import cpw.mods.fml.common.registry.EntityRegistry;
 import dgrxf.watercraft.Watercraft;
 import dgrxf.watercraft.entity.boat.DumbBoat;
-import dgrxf.watercraft.entity.boat.WCEntityBoatBase;
+import dgrxf.watercraft.entity.boat.VanillaBoat;
 
 /**
  * Class Made By: Gory_Moon
@@ -15,7 +15,7 @@ import dgrxf.watercraft.entity.boat.WCEntityBoatBase;
 public class Entities {
     
     public static void init() {
-    	EntityRegistry.registerModEntity(WCEntityBoatBase.class, "WCEntityBoatBase", 0, Watercraft.instance, 80, 3, true);
+    	EntityRegistry.registerModEntity(VanillaBoat.class, "VanillaBoat", 0, Watercraft.instance, 80, 3, true);
         EntityRegistry.registerModEntity(DumbBoat.class, "WCEntityBoat", 1, Watercraft.instance, 80, 3, true);
         /*EntityRegistry.registerModEntity(WCEntitySmartBoat.class, "WCEntitySmartBoat", 2, Watercraft.instance, 80, 3, true);
         EntityRegistry.registerModEntity(EntityBoatChest.class, "EntityBoatChest", 3, Watercraft.instance, 80, 3, true);
