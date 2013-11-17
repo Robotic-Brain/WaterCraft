590f89417a6458932d09cf3783fe71b8a44bdbfe
FrodCube <frodcube@gmail.com>
2013-11-17 21:58:45 +0100

    Started working on the lock GUI

--- WaterCraft/src/dgrxf/watercraft/block/LockAssemblerBlock.java
+++ WaterCraft/src/dgrxf/watercraft/block/LockAssemblerBlock.java
@@ -2,6 +2,7 @@ package dgrxf.watercraft.block;
 
 import net.minecraft.block.BlockContainer;
 import net.minecraft.block.material.Material;
+import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.world.World;
 import dgrxf.watercraft.Watercraft;
@@ -19,6 +20,11 @@ public class LockAssemblerBlock extends BlockContainer {
 	@Override
 	public TileEntity createNewTileEntity(World world) {
 		return new WCTileEntityLockAssembler();
+	}
+	
+	@Override
+	public boolean onBlockActivated(World world, int x, int y, int z, EntityPlayer player, int par6, float par7, float par8, float par9) {
+		return false;
 	}
 
 }
--- WaterCraft/src/dgrxf/watercraft/tileentity/WCTileEntityLockAssembler.java
+++ WaterCraft/src/dgrxf/watercraft/tileentity/WCTileEntityLockAssembler.java
@@ -1,11 +1,91 @@
 package dgrxf.watercraft.tileentity;
 
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.inventory.IInventory;
+import net.minecraft.item.ItemStack;
 import net.minecraft.tileentity.TileEntity;
 
-public class WCTileEntityLockAssembler extends TileEntity /*implements IInventory*/ {
+public class WCTileEntityLockAssembler extends TileEntity implements IInventory {
+	
+	private ItemStack[] items = new ItemStack[5];
 	
 	public WCTileEntityLockAssembler() {
 		
+	}
+
+	@Override
+	public int getSizeInventory() {
+		return items.length;
+	}
+
+	@Override
+	public ItemStack getStackInSlot(int i) {
+		return items[i];
+	}
+
+	@Override
+	public ItemStack decrStackSize(int i, int count) {
+		ItemStack stack = getStackInSlot(i);
+		
+		if (stack != null) {
+			if (stack.stackSize <= count) {
+				setInventorySlotContents(i, null);
+			} else {
+				stack = stack.splitStack(count);
+				onInventoryChanged();
+			}
+		}
+		
+		return stack;
+	}
+
+	@Override
+	public ItemStack getStackInSlotOnClosing(int i) {
+		ItemStack item = getStackInSlot(i);
+		setInventorySlotContents(i, null);
+		return item;
+	}
+
+	@Override
+	public void setInventorySlotContents(int i, ItemStack itemstack) {		
+		if (itemstack != null && itemstack.stackSize > getInventoryStackLimit()) {
+			itemstack.stackSize = getInventoryStackLimit();
+		}
+		
+		items[i] = itemstack;		
+		onInventoryChanged();
+	}
+
+	@Override
+	public String getInvName() {
+		return "Lock Assembler";
+	}
+
+	@Override
+	public boolean isInvNameLocalized() {
+		return false;
+	}
+
+	@Override
+	public int getInventoryStackLimit() {
+		return 64;
+	}
+
+	@Override
+	public boolean isUseableByPlayer(EntityPlayer entityplayer) {
+		return entityplayer.getDistanceSq(xCoord + 0.5, yCoord + 0.5, zCoord + 0.5) <= 64;
+	}
+
+	@Override
+	public void openChest() {}
+
+	@Override
+	public void closeChest() {}
+
+	@Override
+	public boolean isItemValidForSlot(int i, ItemStack itemstack) {
+		// TODO Auto-generated method stub
+		return false;
 	}
 
 }
