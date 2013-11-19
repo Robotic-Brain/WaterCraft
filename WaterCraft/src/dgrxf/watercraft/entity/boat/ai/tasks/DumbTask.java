a8f1ee127e31a1467a690c731124c9d22c470707
Robotic-Brain <github@roboticbrain.de>
2013-11-19 01:35:27 +0100

    Fixing boat motion

--- WaterCraft/src/dgrxf/watercraft/entity/boat/ai/tasks/DumbTask.java
+++ WaterCraft/src/dgrxf/watercraft/entity/boat/ai/tasks/DumbTask.java
@@ -3,7 +3,6 @@ package dgrxf.watercraft.entity.boat.ai.tasks;
 import dgrxf.watercraft.entity.boat.AbstractBaseBoat;
 import dgrxf.watercraft.tileentity.buoy.WCBouyLogic;
 import dgrxf.watercraft.util.LogHelper;
-import dgrxf.watercraft.util.MathHelper;
 import dgrxf.watercraft.util.Vector2;
 
 public class DumbTask extends BoatAITaskBase {
@@ -27,10 +26,9 @@ public class DumbTask extends BoatAITaskBase {
         }
     }
     
-    private static final double BOAT_SPEED = 0.05;
+    private static final double BOAT_SPEED = 0.3;
     
     private void moveToTarget() {
-        //double xDist, zDist;
         if (target == null || boat.worldObj.isRemote) {
             return;
         }
@@ -39,47 +37,10 @@ public class DumbTask extends BoatAITaskBase {
         Vector2 distance = target.sub(boatPos);
         Vector2 newMotion = new Vector2();
         
-        if (distance.length2() > 25.0) {
-            LogHelper.debug("Distance " + distance);
-            //LogHelper.debug("Normalized " + distance.normalize());
-            newMotion = distance.normalize().scalarMult(BOAT_SPEED*2.0);
-        } else {
-            newMotion = distance.normalize().scalarMult(BOAT_SPEED/2.0);
-        }
+        newMotion = distance.normalize().scalarMult(BOAT_SPEED);
         
-        if (distance.dot(new Vector2(boat.motionX, boat.motionZ)) < 0) {
-            newMotion = distance.normalize().scalarMult(1);
-        }
-        
-        /*LogHelper.debug("Boat AI " + newMotion);
-        LogHelper.debug("Target " + target);*/
-        
-        //newMotion.add(new Vector2(boat.motionX, boat.motionZ));
-        
-        boat.motionX += newMotion.x;
-        boat.motionZ += newMotion.y;
-        
-        /*xDist = MathHelper.calculatePointDistance(boat.posX, target.x);
-        zDist = MathHelper.calculatePointDistance(boat.posZ, target.y);
-        if (xDist > 1.0F) {
-            if (boat.posX < target.x) {
-                boat.motionX += BOAT_SPEED;
-            } else if (boat.posX > target.x) {
-                boat.motionX += -BOAT_SPEED;
-            }
-        }
-        
-        if (zDist > 1.0F) {
-            if (boat.posZ < target.y) {
-                boat.motionZ += BOAT_SPEED;
-            } else if (boat.posZ > target.y) {
-                boat.motionZ += -BOAT_SPEED;
-            }
-        }
-        
-        if (zDist <= 1F && xDist <= 1F) {
-            target = null;
-        }*/
+        boat.motionX = newMotion.x;
+        boat.motionZ = newMotion.y;
     }
     
     private void setTargetLocation(Vector2 target) {
