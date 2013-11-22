0d36c97f594e9c5a2ec810c041cb7b1a4676c3f9
Robotic-Brain <github@roboticbrain.de>
2013-11-22 04:13:37 +0100

    Fixing a config derp from 7edbff6ba5d1eb1abc57abe3dac04eb66fe9fbc7 also fixing items
    Items and blocks autoresolve conflicts now

--- WaterCraft/src/dgrxf/watercraft/config/ConfigurationHandler.java
+++ WaterCraft/src/dgrxf/watercraft/config/ConfigurationHandler.java
@@ -25,35 +25,36 @@ public class ConfigurationHandler {
             config.load();
             
             // Items
-            ItemInfo.DUMB_BOAT_ID = config.get(ItemInfo.CATEGORY, ItemInfo.DUMB_BOAT_KEY, ItemInfo.DUMB_BOAT_ID_DEFAULT).getInt() - 256;
-            ItemInfo.BOAT_ID = config.get(ItemInfo.CATEGORY, ItemInfo.BOAT_KEY, ItemInfo.BOAT_ID_DEFAULT).getInt() - 256;
-            ItemInfo.TAPE_MEASURE_ID = config.get(ItemInfo.CATEGORY, ItemInfo.TAPE_MEASURE_KEY, ItemInfo.TAPE_MEASURE_ID_DEFAULT).getInt() - 256;
-            ItemInfo.TELESCOPE_ID = config.get(ItemInfo.CATEGORY, ItemInfo.TELESCOPE_KEY, ItemInfo.TELESCOPE_ID_DEFAULT).getInt() - 256;
-            ItemInfo.ENGINE_ID = config.get(ItemInfo.CATEGORY, ItemInfo.ENGINE_KEY, ItemInfo.ENGINE_ID_DEFAULT).getInt() - 256;
-            ItemInfo.BOATCHEST_ID = config.get(ItemInfo.CATEGORY, ItemInfo.BOATCHEST_KEY, ItemInfo.BOATCHEST_ID_DEFAULT).getInt() - 256;
-            ItemInfo.LAVABOAT_ID = config.get(ItemInfo.CATEGORY, ItemInfo.LAVABOAT_KEY, ItemInfo.LAVABOAT_ID_DEFAULT).getInt() - 256;
-            ItemInfo.FLAG_ID = config.get(ItemInfo.CATEGORY, ItemInfo.FLAG_KEY, ItemInfo.FLAG_ID_DEFAULT).getInt() - 256;
-            ItemInfo.ICEBOAT_ID = config.get(ItemInfo.CATEGORY, ItemInfo.ICEBOAT_KEY, ItemInfo.ICEBOAT_ID_DEFAULT).getInt() - 256;
-            ItemInfo.ROPE_ID = config.get(ItemInfo.CATEGORY, ItemInfo.ROPE_KEY, ItemInfo.ROPE_ID_DEFAULT).getInt() - 256;
-            ItemInfo.PADLOCK_ID = config.get(ItemInfo.CATEGORY, ItemInfo.PADLOCK_KEY, ItemInfo.PADLOCK_ID_DEFAULT).getInt() - 256;
-            ItemInfo.CALCULATOR_ID = config.get(ItemInfo.CATEGORY, ItemInfo.CALCULATOR_KEY, ItemInfo.CALCULATOR_ID_DEFAULT).getInt() - 256;
-            ItemInfo.KEY_ID = config.get(ItemInfo.CATEGORY, ItemInfo.KEY_KEY, ItemInfo.KEY_ID_DEFAULT).getInt() - 256;
-            ItemInfo.TANK_BOAT_ID = config.get(ItemInfo.CATEGORY, ItemInfo.TANK_BOAT_KEY, ItemInfo.TANK_BOAT_ID_DEFAULT).getInt() - 256;
-            ItemInfo.UPGRADE_ID = config.get(ItemInfo.CATEGORY, ItemInfo.UPGRADE_KEY, ItemInfo.UPGRADE_ID_DEFAULT).getInt() - 256;
-            ItemInfo.MODULAR_BOAT_ID = config.get(ItemInfo.CATEGORY, ItemInfo.BOAT_MODULAR_KEY, ItemInfo.MODULAR_BOAT_ID_DEFAULT).getInt() - 256;
+            ItemInfo.DUMB_BOAT_ID = config.getItem(ItemInfo.CATEGORY, ItemInfo.DUMB_BOAT_KEY, ItemInfo.DUMB_BOAT_ID_DEFAULT).getInt();
+            ItemInfo.BOAT_ID = config.getItem(ItemInfo.CATEGORY, ItemInfo.BOAT_KEY, ItemInfo.BOAT_ID_DEFAULT).getInt();
+            ItemInfo.TAPE_MEASURE_ID = config.getItem(ItemInfo.CATEGORY, ItemInfo.TAPE_MEASURE_KEY, ItemInfo.TAPE_MEASURE_ID_DEFAULT).getInt();
+            ItemInfo.TELESCOPE_ID = config.getItem(ItemInfo.CATEGORY, ItemInfo.TELESCOPE_KEY, ItemInfo.TELESCOPE_ID_DEFAULT).getInt();
+            ItemInfo.ENGINE_ID = config.getItem(ItemInfo.CATEGORY, ItemInfo.ENGINE_KEY, ItemInfo.ENGINE_ID_DEFAULT).getInt();
+            ItemInfo.BOATCHEST_ID = config.getItem(ItemInfo.CATEGORY, ItemInfo.BOATCHEST_KEY, ItemInfo.BOATCHEST_ID_DEFAULT).getInt();
+            ItemInfo.LAVABOAT_ID = config.getItem(ItemInfo.CATEGORY, ItemInfo.LAVABOAT_KEY, ItemInfo.LAVABOAT_ID_DEFAULT).getInt();
+            ItemInfo.FLAG_ID = config.getItem(ItemInfo.CATEGORY, ItemInfo.FLAG_KEY, ItemInfo.FLAG_ID_DEFAULT).getInt();
+            ItemInfo.ICEBOAT_ID = config.getItem(ItemInfo.CATEGORY, ItemInfo.ICEBOAT_KEY, ItemInfo.ICEBOAT_ID_DEFAULT).getInt();
+            ItemInfo.ROPE_ID = config.getItem(ItemInfo.CATEGORY, ItemInfo.ROPE_KEY, ItemInfo.ROPE_ID_DEFAULT).getInt();
+            ItemInfo.PADLOCK_ID = config.getItem(ItemInfo.CATEGORY, ItemInfo.PADLOCK_KEY, ItemInfo.PADLOCK_ID_DEFAULT).getInt();
+            ItemInfo.CALCULATOR_ID = config.getItem(ItemInfo.CATEGORY, ItemInfo.CALCULATOR_KEY, ItemInfo.CALCULATOR_ID_DEFAULT).getInt();
+            ItemInfo.KEY_ID = config.getItem(ItemInfo.CATEGORY, ItemInfo.KEY_KEY, ItemInfo.KEY_ID_DEFAULT).getInt();
+            ItemInfo.TANK_BOAT_ID = config.getItem(ItemInfo.CATEGORY, ItemInfo.TANK_BOAT_KEY, ItemInfo.TANK_BOAT_ID_DEFAULT).getInt();
+            ItemInfo.UPGRADE_ID = config.getItem(ItemInfo.CATEGORY, ItemInfo.UPGRADE_KEY, ItemInfo.UPGRADE_ID_DEFAULT).getInt();
+            ItemInfo.MODULAR_BOAT_ID = config.getItem(ItemInfo.CATEGORY, ItemInfo.BOAT_MODULAR_KEY, ItemInfo.MODULAR_BOAT_ID_DEFAULT).getInt();
             
             // Blocks
-            BlockInfo.BUOY_ID = config.get(BlockInfo.CATEGORY, BlockInfo.BUOY_KEY, BlockInfo.BUOY_ID_DEFAULT).getInt();
-            BlockInfo.CONTROL_UNIT_DOCK_ID = config.get(BlockInfo.CATEGORY, BlockInfo.CONTROL_UNIT_DOCK_KEY, BlockInfo.CONTROL_UNIT_DOCK_ID_DEFAULT).getInt();
-            BlockInfo.FREEZER_ID = config.get(BlockInfo.CATEGORY, BlockInfo.FREEZER_KEY, BlockInfo.FREEZER_ID_DEFAULT).getInt();
-            BlockInfo.DROPZONE_ID = config.get(BlockInfo.CATEGORY, BlockInfo.DROPZONE_KEY, BlockInfo.DROPZONE_ID_DEFAULT).getInt();
-            BlockInfo.TOOLBOX_ID = config.get(BlockInfo.CATEGORY, BlockInfo.TOOLBOX_KEY, BlockInfo.TOOLBOX_ID_DEFAULT).getInt();
-            BlockInfo.BUOY_FILTER_ID = config.get(BlockInfo.CATEGORY, BlockInfo.BUOY_FILTER_KEY, BlockInfo.BUOY_FILTER_ID_DEFAULT).getInt();
-            BlockInfo.WC_CHEST_ID = config.get(BlockInfo.CATEGORY, BlockInfo.WC_CHEST_KEY, BlockInfo.WC_CHEST_ID_DEFAULT).getInt();
-            BlockInfo.LOCK_ASSEMBLER_ID = config.get(BlockInfo.CATEGORY, BlockInfo.LOCK_ASSEMBLER_KEY, BlockInfo.LOCK_ASSEMBLER_ID_DEFAULT).getInt();
-            BlockInfo.TANK_ID = config.get(BlockInfo.CATEGORY, BlockInfo.TANK_KEY, BlockInfo.TANK_ID_DEFAULT).getInt();
-            BlockInfo.BOAT_ASSEMBLER_ID = config.get(BlockInfo.CATEGORY, BlockInfo.BOAT_ASSEMBLER_KEY, BlockInfo.BOAT_ASSEMBLER_ID_DEFAULT).getInt();
-            BlockInfo.PLATFORM_ID = config.get(BlockInfo.CATEGORY, BlockInfo.PLATFORM_KEY, BlockInfo.PLATFORM_ID_DEFAULT).getInt();
+            BlockInfo.BUOY_ID = config.getBlock(BlockInfo.CATEGORY, BlockInfo.BUOY_KEY, BlockInfo.BUOY_ID_DEFAULT).getInt();
+            BlockInfo.CONTROL_UNIT_DOCK_ID = config.getBlock(BlockInfo.CATEGORY, BlockInfo.CONTROL_UNIT_DOCK_KEY, BlockInfo.CONTROL_UNIT_DOCK_ID_DEFAULT).getInt();
+            BlockInfo.FREEZER_ID = config.getBlock(BlockInfo.CATEGORY, BlockInfo.FREEZER_KEY, BlockInfo.FREEZER_ID_DEFAULT).getInt();
+            BlockInfo.DROPZONE_ID = config.getBlock(BlockInfo.CATEGORY, BlockInfo.DROPZONE_KEY, BlockInfo.DROPZONE_ID_DEFAULT).getInt();
+            BlockInfo.TOOLBOX_ID = config.getBlock(BlockInfo.CATEGORY, BlockInfo.TOOLBOX_KEY, BlockInfo.TOOLBOX_ID_DEFAULT).getInt();
+            BlockInfo.BUOY_FILTER_ID = config.getBlock(BlockInfo.CATEGORY, BlockInfo.BUOY_FILTER_KEY, BlockInfo.BUOY_FILTER_ID_DEFAULT).getInt();
+            BlockInfo.WC_CHEST_ID = config.getBlock(BlockInfo.CATEGORY, BlockInfo.WC_CHEST_KEY, BlockInfo.WC_CHEST_ID_DEFAULT).getInt();
+            BlockInfo.LOCK_ASSEMBLER_ID = config.getBlock(BlockInfo.CATEGORY, BlockInfo.LOCK_ASSEMBLER_KEY, BlockInfo.LOCK_ASSEMBLER_ID_DEFAULT).getInt();
+            BlockInfo.TANK_ID = config.getBlock(BlockInfo.CATEGORY, BlockInfo.TANK_KEY, BlockInfo.TANK_ID_DEFAULT).getInt();
+            BlockInfo.BOAT_ASSEMBLER_ID = config.getBlock(BlockInfo.CATEGORY, BlockInfo.BOAT_ASSEMBLER_KEY, BlockInfo.BOAT_ASSEMBLER_ID_DEFAULT).getInt();
+            BlockInfo.PLATFORM_ID = config.getBlock(BlockInfo.CATEGORY, BlockInfo.PLATFORM_KEY, BlockInfo.PLATFORM_ID_DEFAULT).getInt();
+            BlockInfo.PHANTOM_MULTIBLOCK_ID = config.getBlock(BlockInfo.CATEGORY, BlockInfo.PHANTOM_MULTIBLOCK_KEY, BlockInfo.PHANTOM_MULTIBLOCK_ID_DEFAULT).getInt();
             
             // Misc
             MiscInfo.BOUY_RANGE = config.get(MiscInfo.CATEGORY, MiscInfo.BOUY_KEY, MiscInfo.BOUY_DEFAULT_RANGE).getInt();
