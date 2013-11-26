3c7290a1a77b73f18981b7485ff2d3bb4b7688a2
xandayn <xandayn101@hotmail.com>
2013-11-26 15:59:42 -0700

    Sort of fixed a small bug when placing nonblocks in the guiboatassembler also fixed lighting bug

--- WaterCraft/src/dgrxf/watercraft/client/gui/interfaces/GuiBoatAssembler.java
+++ WaterCraft/src/dgrxf/watercraft/client/gui/interfaces/GuiBoatAssembler.java
@@ -6,6 +6,7 @@ import net.minecraft.client.renderer.RenderHelper;
 import net.minecraft.client.renderer.entity.RenderManager;
 import net.minecraft.entity.player.InventoryPlayer;
 import net.minecraft.inventory.IInventory;
+import net.minecraft.item.ItemBlock;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.ResourceLocation;
 
@@ -43,6 +44,7 @@ public class GuiBoatAssembler extends GuiBase {
 		fontRenderer.drawString(returnItemName(0), 10, 31, GuiColor.WHITE.toRGB());
 		fontRenderer.drawString(returnItemName(1), 103, 31, GuiColor.WHITE.toRGB());
 
+		rotation++;
 		renderItem(53, 75, inventory.getStackInSlot(0));
 		renderItem(145, 75, inventory.getStackInSlot(1));
 	}
@@ -54,27 +56,26 @@ public class GuiBoatAssembler extends GuiBase {
 	
 	public void renderItem(int x, int y, ItemStack stack){
 		if(stack != null){
-			
+
 				GL11.glPushMatrix();
+				RenderHelper.disableStandardItemLighting();
 				GL11.glTranslatef(x, y, 100);
 				
 				float scale = 30F;
 				GL11.glScalef(-scale, scale, scale);
-				
-				RenderHelper.enableStandardItemLighting();
+
 				GL11.glRotatef(180, 0, 0, 1);
 				GL11.glRotatef(20, 1, 0, 0);
 				GL11.glRotatef(rotation, 0, 1, 0);
 				
-				rotation++;
 				if(rotation == 360)
 					rotation = 0;
 				if(stack.getItem() == ModItems.modularBoat)
 					RenderManager.instance.renderEntityWithPosYaw(new ModularBoat(Minecraft.getMinecraft().theWorld, 0, 0, 0, stack.getTagCompound()), 0, 0, 0, 0, 0);
-				else{
+				else if(stack.getItem() instanceof ItemBlock){
 					RenderManager.instance.itemRenderer.renderItem(null, stack, 10);
 				}
-				RenderHelper.disableStandardItemLighting();
+				RenderHelper.enableStandardItemLighting();
 				GL11.glPopMatrix();
 			}
 	}
