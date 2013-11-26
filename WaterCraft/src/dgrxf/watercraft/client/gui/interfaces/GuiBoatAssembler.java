2a7b691b8fc1b1672b21a2dd5e1d4ff7f2f49f88
TheDrunkMafia <sean.hall412@gmail.com>
2013-11-26 22:24:30 +0000

    Started on Rendering code for the GUI

--- WaterCraft/src/dgrxf/watercraft/client/gui/interfaces/GuiBoatAssembler.java
+++ WaterCraft/src/dgrxf/watercraft/client/gui/interfaces/GuiBoatAssembler.java
@@ -1,22 +1,39 @@
 package dgrxf.watercraft.client.gui.interfaces;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
+import net.minecraft.block.Block;
 import net.minecraft.client.Minecraft;
+import net.minecraft.client.renderer.RenderBlocks;
+import net.minecraft.client.renderer.RenderHelper;
+import net.minecraft.client.renderer.entity.RenderManager;
+import net.minecraft.entity.item.EntityFallingSand;
 import net.minecraft.entity.player.InventoryPlayer;
 import net.minecraft.inventory.IInventory;
+import net.minecraft.item.ItemStack;
 import net.minecraft.util.ResourceLocation;
+import net.minecraft.world.IBlockAccess;
 
 import org.lwjgl.opengl.GL11;
 
+import cpw.mods.fml.client.registry.ISimpleBlockRenderingHandler;
+import dgrxf.watercraft.client.gui.GuiColor;
+import dgrxf.watercraft.entity.boat.ModularBoat;
+import dgrxf.watercraft.item.ModItems;
 import dgrxf.watercraft.server.container.BoatAssemblerContainer;
 
-public class GuiBoatAssembler extends GuiBase{
+public class GuiBoatAssembler extends GuiBase {
 	
 	private static final ResourceLocation texture = new ResourceLocation("watercraft", "textures/gui/boatassembler.png"); 
+	private IInventory inventory;
+	private int rotation = 0;
 	
 	public GuiBoatAssembler(InventoryPlayer inventory, IInventory inv) {
 		super(new BoatAssemblerContainer(inventory, inv));
 		this.xSize = 196;
-		this.ySize = 182;
+		this.ySize = 218;
+		this.inventory = inv;
 	}
 
 	@Override
@@ -25,5 +42,58 @@ public class GuiBoatAssembler extends GuiBase{
         Minecraft.getMinecraft().renderEngine.bindTexture(texture);
         drawTexturedModalRect(guiLeft, guiTop, 0, 0, xSize, ySize);
 	}
+	
+	@Override
+	protected void drawGuiContainerForegroundLayer(int x, int y) {
+		GL11.glColor4f(1, 1, 1, 1);
+		fontRenderer.drawString("Boat Assembler", 8, 6, GuiColor.GRAY.toRGB());
+		fontRenderer.drawString(returnItemName(0), 10, 31, GuiColor.WHITE.toRGB());
+		fontRenderer.drawString(returnItemName(1), 103, 31, GuiColor.WHITE.toRGB());
 
+		renderItem(53, 75, inventory.getStackInSlot(0));
+	}
+	
+	public String returnItemName(int slot){
+		ItemStack stack = inventory.getStackInSlot(slot);
+		return stack != null ? stack.getDisplayName() : "Empty";
+	}
+	
+	public void renderItem(int x, int y, ItemStack stack){
+		if(stack != null){
+			if(stack.getItem() == ModItems.modularBoat){
+				GL11.glPushMatrix();
+				GL11.glTranslatef(x, y, 100);
+				
+				float scale = 30F;
+				GL11.glScalef(-scale, scale, scale);
+				
+				RenderHelper.enableStandardItemLighting();
+				GL11.glRotatef(180, 0, 0, 1);
+				GL11.glRotatef(20, 1, 0, 0);
+				GL11.glRotatef(rotation, 0, 1, 0);
+				
+				rotation++;
+				if(rotation == 360)
+					rotation = 0;
+				
+				RenderManager.instance.renderEntityWithPosYaw(new ModularBoat(Minecraft.getMinecraft().theWorld, 0, 0, 0, stack.getTagCompound()), 0, 0, 0, 0, 0);
+
+				RenderHelper.disableStandardItemLighting();
+				GL11.glPopMatrix();
+			}else if(isItemBlock(stack.getItem().itemID)){
+				GL11.glPushMatrix();
+
+				GL11.glPopMatrix();
+			}
+		}
+	}
+	
+	public boolean isItemBlock(int id){
+		
+		if(id < Block.blocksList.length){
+			System.out.println("id");
+			return Block.blocksList[id] != null ? true : false;
+		}
+		return false;
+	}
 }
