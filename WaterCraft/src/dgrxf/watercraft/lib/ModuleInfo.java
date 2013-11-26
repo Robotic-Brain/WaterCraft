c2d00cf70953001314b0e899b4ba817b9445b907
xandayn <xandayn101@hotmail.com>
2013-11-25 19:53:13 -0700

    Just a bit of tweaking to ModuleRegistry

--- WaterCraft/src/dgrxf/watercraft/lib/ModuleInfo.java
+++ WaterCraft/src/dgrxf/watercraft/lib/ModuleInfo.java
@@ -17,11 +17,11 @@ import dgrxf.watercraft.module.ModuleRegistry;
 
 public class ModuleInfo {
 	public static void init(){
-		ModuleRegistry.registerModule(new ItemStack(ModItems.boatModule, 0, 0), new CustomModule(new ModuleType[]{ModuleType.BOAT}, new ModuleType[]{ModuleType.BOAT}, null, DumbTask.class));
-		ModuleRegistry.registerModule(new ItemStack(ModItems.boatModule, 0, 1), new CustomModule(new ModuleType[]{ModuleType.BOAT, ModuleType.ACTIVATABLE}, new ModuleType[]{ModuleType.BOAT, ModuleType.BLOCK}, null, VanillaTask.class));
-		ModuleRegistry.registerModule(new ItemStack(ModItems.boatModule, 0, 2), new CustomModule(new ModuleType[]{ModuleType.AI}, new ModuleType[0], null, IceTask.class));
-		ModuleRegistry.registerModule(new ItemStack(ModItems.boatModule, 0, 3), new CustomModule(new ModuleType[]{ModuleType.AI}, new ModuleType[0], null, LavaTask.class));
-		ModuleRegistry.registerModule(new ItemStack(Block.dirt), new CustomModule(new ModuleType[0], new ModuleType[0], Block.dirt, null));
-		ModuleRegistry.registerModule(new ItemStack(Block.chest), new CustomModule(new ModuleType[]{ModuleType.INVENTORY, ModuleType.ACTIVATABLE}, new ModuleType[]{ModuleType.INVENTORY, ModuleType.TANK}, ModBlocks.chest, InventoryTask.class, GuiHandler.VANILLA_CHEST_ID, Watercraft.instance, 27, true));
+		ModuleRegistry.registerModule(new ItemStack(ModItems.boatModule, 0, 0), new CustomModule(new ModuleType[]{ModuleType.BOAT}, new ModuleType[]{ModuleType.BOAT}, null, DumbTask.class), Watercraft.instance);
+		ModuleRegistry.registerModule(new ItemStack(ModItems.boatModule, 0, 1), new CustomModule(new ModuleType[]{ModuleType.BOAT, ModuleType.ACTIVATABLE}, new ModuleType[]{ModuleType.BOAT, ModuleType.BLOCK}, null, VanillaTask.class), Watercraft.instance);
+		ModuleRegistry.registerModule(new ItemStack(ModItems.boatModule, 0, 2), new CustomModule(new ModuleType[]{ModuleType.AI}, new ModuleType[0], null, IceTask.class), Watercraft.instance);
+		ModuleRegistry.registerModule(new ItemStack(ModItems.boatModule, 0, 3), new CustomModule(new ModuleType[]{ModuleType.AI}, new ModuleType[0], null, LavaTask.class), Watercraft.instance);
+		ModuleRegistry.registerModule(new ItemStack(Block.dirt), new CustomModule(new ModuleType[0], new ModuleType[0], Block.dirt, null), Watercraft.instance);
+		ModuleRegistry.registerModule(new ItemStack(Block.chest), new CustomModule(new ModuleType[]{ModuleType.INVENTORY, ModuleType.ACTIVATABLE}, new ModuleType[]{ModuleType.INVENTORY, ModuleType.TANK}, ModBlocks.chest, InventoryTask.class, GuiHandler.VANILLA_CHEST_ID, Watercraft.instance, 27, true), Watercraft.instance);
 	}
 }
--- WaterCraft/src/dgrxf/watercraft/module/ModuleRegistry.java
+++ WaterCraft/src/dgrxf/watercraft/module/ModuleRegistry.java
@@ -4,11 +4,16 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.HashSet;
 
+import cpw.mods.fml.client.FMLFileResourcePack;
+import cpw.mods.fml.common.FMLCommonHandler;
+import cpw.mods.fml.common.FMLModContainer;
+import cpw.mods.fml.common.event.FMLInterModComms;
 import net.minecraft.block.Block;
 import net.minecraft.client.Minecraft;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NBTTagCompound;
+import net.minecraftforge.common.ForgeVersion;
 import dgrxf.watercraft.entity.boat.AbstractBaseBoat;
 import dgrxf.watercraft.entity.boat.ai.BoatAITaskList;
 import dgrxf.watercraft.enumeration.Alphabet;
@@ -19,6 +24,7 @@ import dgrxf.watercraft.util.LogHelper;
 public class ModuleRegistry {
 	
 	private static HashMap<Integer, ItemStack> registeredModules = new HashMap();
+	private static HashMap<Integer, Object> modByID = new HashMap();
 	protected static HashMap<Integer, IBoatModule> modules = new HashMap();
 	private static ArrayList<Integer> registeredIDs = new ArrayList();
 	
@@ -27,14 +33,16 @@ public class ModuleRegistry {
 	 * @param mod The module you wish to register.
 	 * @return True if the module was registered successfully, false if not.
 	 */
-	public static final boolean registerModule(ItemStack itemStack, IBoatModule mod){		
-		if(isItemRegistered(itemStack)){
+	public static final boolean registerModule(ItemStack itemStack, IBoatModule mod, Object modID){
+		int temp = isItemRegisteredAndGetID(itemStack);
+		if(temp != -1){
 			try {
-				throw new ModuleException("You are attempting to register a module with to the item named \"" + itemStack.getDisplayName() + "\", however, this item is already in use and cannot be overwritten.");
+				throw new ModuleException("The mod " + modID.getClass().getSimpleName() + " is attempting to register a module with to the item named \"" + itemStack.getDisplayName() + "\", however, this item is already registered by " + getModulesMod(temp).getClass().getSimpleName() + " and cannot be overwritten.");
 			} catch (ModuleException e) {
 				e.printStackTrace();
 			}
 		}else{
+			modByID.put(getNextID(), modID);
 			registeredModules.put(getNextID(), itemStack);
 			modules.put(getNextID(), mod);
 			registeredIDs.add(getNextID());
@@ -46,7 +54,11 @@ public class ModuleRegistry {
 	private static int getNextID(){
 		return registeredIDs.size();
 	}
-
+	
+	private static Object getModulesMod(int id){
+		return modByID.get(id);
+	}
+	
 	/**
 	 * @param item The item you wish to see if it has been associated with a module
 	 * @return True if the item has an associated module
