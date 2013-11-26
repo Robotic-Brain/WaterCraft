95ebca87411f3a611502dc99b04a03d1b9267ac3
TheDrunkMafia <sean.hall412@gmail.com>
2013-11-26 20:05:38 +0000

    Added Translation

--- WaterCraft/src/dgrxf/watercraft/util/TranslationHelper.java
+++ WaterCraft/src/dgrxf/watercraft/util/TranslationHelper.java
@@ -16,9 +16,10 @@ public class TranslationHelper {
     /**
      * Translation Keys
      */
-    public static final String TAPE_MEASURE_START      = BASE_KEY + "tape_measure_start";
-    public static final String TAPE_MEASURE_END        = BASE_KEY + "tape_measure_end";
-    public static final String TAPE_MEASURE_DISTANCE   = BASE_KEY + "tape_measure_distance";
+    public static final String TAPE_MEASURE_START        = BASE_KEY + "tape_measure_start";
+    public static final String TAPE_MEASURE_END          = BASE_KEY + "tape_measure_end";
+    public static final String TAPE_MEASURE_DISTANCE     = BASE_KEY + "tape_measure_distance";
+    public static final String TAPE_MEASURE_RESTART      = BASE_KEY + "tape_measure_restart";
     
     public static final String TOOLBOX_CONTAINS        = BASE_KEY + "toolbox_contains";
     public static final String TOOLBOX_CONTAINS_NOTING = BASE_KEY + "toolbox_contains_nothing";
