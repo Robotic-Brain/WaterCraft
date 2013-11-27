a2d5c3c0d343a80ba9a3839cbcf8f4006b642473
TheDrunkMafia <sean.hall412@gmail.com>
2013-11-27 15:27:19 +0000

    The start of something new

---
+++ WaterCraft/src/dgrxf/watercraft/client/gui/interfaces/GUI3DRectangle.java
@@ -0,0 +1,10 @@
+package dgrxf.watercraft.client.gui.interfaces;
+
+public class GUI3DRectangle extends GuiRectangle{
+
+	public GUI3DRectangle(int x, int y, int w, int h) {
+		super(x, y, w, h);
+		
+	}
+
+}
