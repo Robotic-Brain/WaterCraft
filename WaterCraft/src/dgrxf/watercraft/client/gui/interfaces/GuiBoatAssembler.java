9a6bd3cfd929efb77b56eb725ef584fdba794764
xandayn <xandayn101@hotmail.com>
2013-11-26 18:00:48 -0700

    Fixed up the rendering code, it works now, but still needs work to make it look correct.

--- WaterCraft/src/dgrxf/watercraft/client/gui/interfaces/GuiBoatAssembler.java
+++ WaterCraft/src/dgrxf/watercraft/client/gui/interfaces/GuiBoatAssembler.java
@@ -2,16 +2,21 @@ package dgrxf.watercraft.client.gui.interfaces;
 
 import net.minecraft.block.Block;
 import net.minecraft.client.Minecraft;
+import net.minecraft.client.renderer.ItemRenderer;
+import net.minecraft.client.renderer.RenderBlocks;
 import net.minecraft.client.renderer.RenderHelper;
+import net.minecraft.client.renderer.Tessellator;
 import net.minecraft.client.renderer.entity.RenderManager;
+import net.minecraft.client.renderer.texture.TextureManager;
 import net.minecraft.entity.player.InventoryPlayer;
 import net.minecraft.inventory.IInventory;
 import net.minecraft.item.ItemBlock;
 import net.minecraft.item.ItemStack;
+import net.minecraft.util.Icon;
 import net.minecraft.util.ResourceLocation;
-import net.minecraftforge.client.IItemRenderer.ItemRenderType;
 
 import org.lwjgl.opengl.GL11;
+import org.lwjgl.opengl.GL12;
 
 import dgrxf.watercraft.client.gui.GuiColor;
 import dgrxf.watercraft.entity.boat.ModularBoat;
@@ -62,29 +67,64 @@ public class GuiBoatAssembler extends GuiBase {
 
 				GL11.glPushMatrix();
 				RenderHelper.disableStandardItemLighting();
-				GL11.glTranslatef(x, y, 100);
-				
-				float scale = 30F;
-				GL11.glScalef(-scale, scale, scale);
-
-				GL11.glRotatef(180, 0, 0, 1);
-				GL11.glRotatef(20, 1, 0, 0);
-				GL11.glRotatef(rotation, 0, 1, 0);
 				
 				if(rotation == 360)
 					rotation = 0;
-				if(stack.getItem() == ModItems.modularBoat)
+				
+				if(stack.getItem() == ModItems.modularBoat){
+					GL11.glTranslatef(x, y, 100);
+					float scale = 30F;
+					GL11.glScalef(-scale, scale, scale);
+					GL11.glRotatef(180, 0, 0, 1);
+					GL11.glRotatef(20, 1, 0, 0);
+					GL11.glRotatef(rotation, 0, 1, 0);
 					RenderManager.instance.renderEntityWithPosYaw(new ModularBoat(Minecraft.getMinecraft().theWorld, 0, 0, 0, stack.getTagCompound()), 0, 0, 0, 0, 0);
-				else {
-					RenderManager.instance.itemRenderer.renderItem(playerInv.player, stack, 10);
+				}else if(stack.getItem() instanceof ItemBlock){
+					renderBlock(stack, x, y, 30F);
+				}else {
+					renderItem(stack, x, y);
 				}
 				RenderHelper.enableStandardItemLighting();
 				GL11.glPopMatrix();
 			}
 	}
 	
-	
-	
+	private void renderItem(ItemStack stack, int x, int y) {
+        Icon icon = playerInv.player.getItemIcon(stack, stack.getItemDamage());
+        TextureManager texturemanager = Minecraft.getMinecraft().renderEngine;
+
+        if (icon == null)
+        {
+            GL11.glPopMatrix();
+            return;
+        }
+        
+		texturemanager.bindTexture(texturemanager.getResourceLocation(stack.getItemSpriteNumber()));
+        Tessellator tessellator = Tessellator.instance;
+        float f = icon.getMinU();
+        float f1 = icon.getMaxU();
+        float f2 = icon.getMinV();
+        float f3 = icon.getMaxV();
+        GL11.glTranslatef(x, y, 100.0F);
+        GL11.glRotatef(rotation, 0, 1, 0);
+        GL11.glRotatef(180, 0, 0, 1);
+        GL11.glScalef(-30, 30, 30);
+        ItemRenderer.renderItemIn2D(tessellator, f1, f2, f, f3, icon.getIconWidth(), icon.getIconHeight()-10, 0.0625F);
+	}
+
+	private void renderBlock(ItemStack stack, int x, int y, float scale) {
+		TextureManager textMan = Minecraft.getMinecraft().getTextureManager();
+		RenderBlocks renderBlocks = new RenderBlocks();
+		
+		textMan.bindTexture(textMan.getResourceLocation(0));
+		GL11.glTranslatef(x, y, 100);
+		GL11.glScalef(-scale, scale, scale);
+		GL11.glRotatef(180, 0, 0, 1);
+		GL11.glRotatef(20, 1, 0, 0);
+		GL11.glRotatef(rotation, 0, 1, 0);
+		renderBlocks.renderBlockAsItem(Block.blocksList[stack.itemID], stack.getItemDamage(), 1.0f);
+	}
+
 	public boolean isItemBlock(int id){
 		
 		if(id < Block.blocksList.length){
