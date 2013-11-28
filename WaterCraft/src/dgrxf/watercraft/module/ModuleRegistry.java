8bb25880e648aeccc58819526416f25a6e3b733c
xandayn <xandayn101@hotmail.com>
2013-11-28 12:22:57 -0700

    Modified the module registry so that it now saves a hashmap of mod containers instead of objects, I feel this will be useful somehow

--- WaterCraft/src/dgrxf/watercraft/module/ModuleRegistry.java
+++ WaterCraft/src/dgrxf/watercraft/module/ModuleRegistry.java
@@ -4,12 +4,14 @@ import java.util.ArrayList;
 import java.util.HashMap;
 
 import net.minecraft.item.ItemStack;
+import cpw.mods.fml.common.FMLCommonHandler;
+import cpw.mods.fml.common.ModContainer;
 import dgrxf.watercraft.interfaces.IBoatModule;
 
 public class ModuleRegistry {
 	
 	private static HashMap<Integer, ItemStack> registeredModules = new HashMap();
-	private static HashMap<Integer, Object> modByID = new HashMap();
+	private static HashMap<Integer, ModContainer> modByID = new HashMap();
 	protected static HashMap<Integer, IBoatModule> modules = new HashMap();
 	private static ArrayList<Integer> registeredIDs = new ArrayList();
 	
@@ -20,27 +22,38 @@ public class ModuleRegistry {
 	 */
 	public static final boolean registerModule(ItemStack itemStack, IBoatModule mod, Object modID){
 		int temp = isItemRegisteredAndGetID(itemStack);
-		if(temp != -1){
-			try {
-				throw new RuntimeException("The mod " + modID.getClass().getSimpleName() + " is attempting to register a module with to the item named \"" + itemStack.getDisplayName() + "\", however, this item is already registered by " + getModulesMod(temp).getClass().getSimpleName() + " and cannot be overwritten.");
-			} catch (RuntimeException e) {
-				e.printStackTrace();
+
+		ModContainer m = FMLCommonHandler.instance().findContainerFor(modID);
+		if(m != null){
+			if(temp != -1){
+				try {
+					throw new RuntimeException("The mod " + m.getName() + " is attempting to register a module with to the item named \"" + itemStack.getDisplayName() + "\", however, this item is already registered by " + getModulesMod(temp).getName() + " and cannot be overwritten.");
+				} catch (RuntimeException e) {
+					e.printStackTrace();
+				}
+			}else{
+				modByID.put(getNextID(), m);
+				registeredModules.put(getNextID(), itemStack);
+				modules.put(getNextID(), mod);
+				registeredIDs.add(getNextID());
+				return true;
 			}
+			return false;
 		}else{
-			modByID.put(getNextID(), modID);
-			registeredModules.put(getNextID(), itemStack);
-			modules.put(getNextID(), mod);
-			registeredIDs.add(getNextID());
-			return true;
+			try{
+				throw new RuntimeException("The class " + modID.getClass().getSimpleName() + " is not a registered Forge Modloader mod container, the module with the item " + itemStack.getDisplayName() + " will not be registered.");
+			} catch(RuntimeException e){
+				e.printStackTrace();
+				return false;
+			}
 		}
-		return false;
 	}
 	
 	private static int getNextID(){
 		return registeredIDs.size();
 	}
 	
-	private static Object getModulesMod(int id){
+	public static ModContainer getModulesMod(int id){
 		return modByID.get(id);
 	}
 	
