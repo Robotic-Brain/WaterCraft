2cedc513419c5196e2e4b169b06ad2107fb3cf81
TheDrunkMafia <sean.hall412@gmail.com>
2013-12-04 16:18:18 +0000

    Working on boat GUI

--- WaterCraft/src/dgrxf/watercraft/client/gui/boat/GuiBoat.java
+++ WaterCraft/src/dgrxf/watercraft/client/gui/boat/GuiBoat.java
@@ -1,6 +1,10 @@
 package dgrxf.watercraft.client.gui.boat;
 
+import org.lwjgl.opengl.GL11;
+
+import net.minecraft.client.Minecraft;
 import net.minecraft.inventory.IInventory;
+import net.minecraft.util.ResourceLocation;
 import dgrxf.watercraft.client.gui.GuiBase;
 import dgrxf.watercraft.common.container.BoatContainer;
 import dgrxf.watercraft.entity.boat.AbstractBaseBoat;
@@ -16,11 +20,17 @@ public class GuiBoat extends GuiBase{
 
 	public GuiBoat(IInventory player, AbstractBaseBoat boat) {
 		super(new BoatContainer(player, boat));
+		xSize = 196;
+        ySize = 218;
 	}
-
+	
+	private static final ResourceLocation texture = new ResourceLocation("watercraft", "textures/gui/boat.png");
+	
 	@Override
 	protected void drawGuiContainerBackgroundLayer(float f, int x, int y) {
-		
+		 GL11.glColor4f(1, 1, 1, 1);
+	     Minecraft.getMinecraft().renderEngine.bindTexture(texture);
+	     drawTexturedModalRect(guiLeft, guiTop, 0, 0, xSize, ySize);
 	}
 	
 	@Override
