f44de165e9528a2b02c1753b566f9b4b3c22277d
TheDrunkMafia <sean.hall412@gmail.com>
2013-12-05 14:16:20 +0000

    Boat Gui W.I.P

--- WaterCraft/src/dgrxf/watercraft/client/gui/boat/GuiBoat.java
+++ WaterCraft/src/dgrxf/watercraft/client/gui/boat/GuiBoat.java
@@ -1,13 +1,22 @@
 package dgrxf.watercraft.client.gui.boat;
 
+import java.util.ArrayList;
+
 import org.lwjgl.opengl.GL11;
 
 import net.minecraft.client.Minecraft;
 import net.minecraft.inventory.IInventory;
+import net.minecraft.item.ItemStack;
 import net.minecraft.util.ResourceLocation;
 import dgrxf.watercraft.client.gui.GuiBase;
+import dgrxf.watercraft.client.gui.GuiColor;
+import dgrxf.watercraft.client.gui.components.GuiDropDownScrollList;
+import dgrxf.watercraft.client.gui.crane.CraneGUI;
 import dgrxf.watercraft.common.container.BoatContainer;
 import dgrxf.watercraft.entity.boat.AbstractBaseBoat;
+import dgrxf.watercraft.enumeration.Colours;
+import dgrxf.watercraft.module.ModuleHelper;
+import dgrxf.watercraft.module.ModuleRegistry;
 
 /**
  * 
@@ -17,14 +26,19 @@ import dgrxf.watercraft.entity.boat.AbstractBaseBoat;
  *
  */
 public class GuiBoat extends GuiBase{
-
+	
+	private AbstractBaseBoat boat;
+	
+	private static final ResourceLocation texture = new ResourceLocation("watercraft", "textures/gui/boat.png");
+	private ArrayList<ItemStack> modules;
+	
 	public GuiBoat(IInventory player, AbstractBaseBoat boat) {
 		super(new BoatContainer(player, boat));
 		xSize = 196;
         ySize = 218;
+        
+        this.boat = boat;
 	}
-	
-	private static final ResourceLocation texture = new ResourceLocation("watercraft", "textures/gui/boat.png");
 	
 	@Override
 	protected void drawGuiContainerBackgroundLayer(float f, int x, int y) {
@@ -35,6 +49,18 @@ public class GuiBoat extends GuiBase{
 	
 	@Override
 	protected void drawGuiContainerForegroundLayer(int x, int y) {
-		
+		fontRenderer.drawString("Boat", 8, 5, GuiColor.GRAY.toRGB());
+	}
+	
+	@Override
+    protected void mouseClicked(int x, int y, int button) {
+	}
+
+	@Override
+    protected void mouseClickMove(int x, int y, int button, long timeSinceClicked) {
+	}
+
+	@Override
+    protected void mouseMovedOrUp(int x, int y, int button) {
 	}
 }
